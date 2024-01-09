75b0c8c632b1dbbb89237889039931081d211a1f
Yoann Fleury <yoann.fleury@yahoo.com>
2024-01-09 21:26:20 +0100

    feat: add linkedin

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -20,6 +20,28 @@ const navigation = [
     ),
   },
   {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/company/fork-it-community",
+    icon: (props: SVGProps<SVGSVGElement>) => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        {...props}
+      >
+        <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+        <rect width="4" height="12" x="2" y="9" />
+        <circle cx="4" cy="4" r="2" />
+      </svg>
+    ),
+  },
+  {
     name: "GitHub",
     href: "https://github.com/Fork-It-Community",
     icon: (props: SVGProps<SVGSVGElement>) => (
