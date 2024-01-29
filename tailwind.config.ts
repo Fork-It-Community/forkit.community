1ecb6d44d008e90617a927660a84c25ffed83242
Ivan Dalmet <ivan@dalmet.fr>
2024-01-29 12:54:19 +0100

    fix: Use tailwind typography for code of conduct

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,7 +84,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
 
 export default config;
