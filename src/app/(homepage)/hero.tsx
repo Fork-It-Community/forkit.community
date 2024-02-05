3df6c18563bfb420958799b660cb3967e8a6cdb4
Ivan Dalmet <ivan@dalmet.fr>
2024-02-05 14:00:08 +0100

    feat: Update emailing form

--- src/app/(homepage)/hero.tsx
+++ src/app/(homepage)/hero.tsx
@@ -44,7 +44,10 @@ export function Hero() {
                 asChild
                 className="w-full sm:w-auto"
               >
-                <a href="https://tally.so/r/wz7zPZ" target="_blank">
+                <a
+                  href="https://7dca2c80.sibforms.com/serve/MUIFALlyE38ui4aH9KjSPGdr7uvQ80DoieNXY0h5mpV12H4Q94-JSLzSLOGcz4TPY1tLHKwqmEDeO2AXBqdyscHubd0EvQHRKsxW0X7NfX4lW8EDDkT5nPjS0UR_NahafQHdCwrVBSjugPgynYeoCjieOottXocinkeXAxeqjpsDhguWcuFWtPNgswPVrkalmEzKWeQN948pTnxB"
+                  target="_blank"
+                >
                   <MailIcon size="1em" className="mr-2" />
                   Keep in touch
                 </a>
