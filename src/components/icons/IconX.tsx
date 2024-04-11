2a1ade8f30b8513578af60d5d4bb47b67524f8ee
Jeanne <jeanne.grenet@gmail.com>
2024-04-11 09:54:22 +0200

    fix icons in the footer

--- src/components/icons/IconX.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-export const IconX = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg fill="currentColor" viewBox="0 0 512 512" {...props}>
-    <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8l164.9-188.5L26.8 48h145.6l100.5 132.9L389.2 48zm-24.8 373.8h39.1L151.1 88h-42l255.3 333.8z" />
-  </svg>
-);
