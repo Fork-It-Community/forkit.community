6e14f104a5fdc10856c53f2d2b26e38f28d7fdc8
Ivan Dalmet <ivan@dalmet.fr>
2024-04-19 11:41:22 +0200

    wip: weekend page

---
+++ src/app/events/[slug]/weekend/page.tsx
@@ -0,0 +1,13 @@
+export default function WeekendPage() {
+  return (
+    <div className="prose prose-sm prose-invert mx-auto w-full p-6 md:prose-base prose-headings:scroll-m-10 prose-headings:font-heading prose-headings:text-primary">
+      <h1>Weekend Guide</h1>
+      <h2>jjjjj</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At atque dolor
+        beatae ut nam ipsum architecto, inventore quia quasi earum. Totam et
+        dolore cupiditate aliquam repellat deserunt nobis obcaecati ipsa.
+      </p>
+    </div>
+  );
+}
