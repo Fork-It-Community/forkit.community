bdb6bdbd16c85d11fbe7205fabb038ae0135c625
Yoann Fleury <yoann.fleury@yahoo.com>
2024-05-21 14:16:39 +0200

    refactor: put the weekend information in an mdx

--- src/components/prose.tsx
+++ src/components/prose.tsx
@@ -6,7 +6,7 @@ export const Prose = (props: HTMLAttributes<HTMLDivElement>) => {
     <div
       {...props}
       className={cn(
-        "prose prose-sm prose-invert mx-auto p-6 md:prose-base prose-headings:scroll-m-10 prose-headings:font-heading prose-headings:text-primary prose-h3:flex prose-h3:items-center prose-h3:gap-2 prose-h3:font-bold prose-h3:text-white",
+        "prose prose-sm prose-invert mx-auto p-6 md:prose-base prose-headings:scroll-m-10 prose-headings:font-heading prose-headings:text-primary prose-h1:mt-6 prose-h3:flex prose-h3:items-center prose-h3:gap-2 prose-h3:font-bold prose-h3:text-white",
         props.className,
       )}
     />
