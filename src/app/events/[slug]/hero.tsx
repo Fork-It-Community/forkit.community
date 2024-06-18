ff2feadcaecacc9de1b5b28fbe0ecd2755ac121b
Yoann Fleury <yoann.fleury@yahoo.com>
2024-06-18 15:08:57 +0200

    style: code improvement

--- src/app/events/[slug]/hero.tsx
+++ src/app/events/[slug]/hero.tsx
@@ -5,8 +5,9 @@ import { ExternalLink } from "lucide-react";
 import Image from "next/image";
 
 const MAX_PRIMARY_BUTTONS = 2;
-const buttonPrimaryClassName = "w-full min-w-[20ch] sm:w-auto";
+
 const buttonSecondaryClassName = "-mx-4";
+
 export function Hero(
   props: Readonly<{
     event: Event;
@@ -62,12 +63,11 @@ export function Hero(
           <p className="text-md mt-6 text-gray-400">{props.event.excerpt}</p>
           <div className="mx-8 mt-10 flex flex-wrap items-center justify-center gap-x-6 gap-y-4">
             {shouldDisplayTicketButton(props.event) && (
-              <Button asChild>
+              <Button asChild variant="default">
                 <a
                   href={props.event.tickets?.href}
                   target="_blank"
                   rel="noreferer"
-                  className={buttonPrimaryClassName}
                 >
                   Get tickets
                   <ExternalLink className="ml-2 h-4 w-4" />
@@ -75,12 +75,11 @@ export function Hero(
               </Button>
             )}
             {isProspectusActive && (
-              <Button variant="outline" asChild>
+              <Button variant="default" asChild>
                 <a
                   href={props.event.prospectus?.href}
                   target="_blank"
                   rel="noreferer"
-                  className={buttonPrimaryClassName}
                 >
                   {props.event.prospectus?.title ?? "Sponsoring prospectus"}
                   <ExternalLink className="ml-2 h-4 w-4" />
@@ -94,20 +93,10 @@ export function Hero(
                   [isProspectusActive, isTicketsActive].filter(Boolean)
                     .length >= MAX_PRIMARY_BUTTONS
                     ? "linkWhite"
-                    : undefined
+                    : "default"
                 }
               >
-                <a
-                  href={props.event.cfp?.href}
-                  target="_blank"
-                  rel="noreferer"
-                  className={
-                    [isProspectusActive, isTicketsActive].filter(Boolean)
-                      .length < MAX_PRIMARY_BUTTONS
-                      ? buttonPrimaryClassName
-                      : buttonSecondaryClassName
-                  }
-                >
+                <a href={props.event.cfp?.href} target="_blank" rel="noreferer">
                   Call For Paper
                   <ExternalLink className="ml-2 h-4 w-4" />
                 </a>
