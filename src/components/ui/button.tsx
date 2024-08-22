33d4aeb60995ac2f154d540566f58a2624faffec
JeanneGrenet <jeanne.grenet@gmail.com>
2024-08-22 10:19:37 +0200

    fix : hover color and focus

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,17 +5,17 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-primary/80",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-black text-primary hover:bg-neutral-900 border border-neutral-700",
+          "bg-black text-primary hover:bg-neutral-800 border border-neutral-700",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary text-sm underline-offset-4 underline p-0 hover:no-underline",
       },
