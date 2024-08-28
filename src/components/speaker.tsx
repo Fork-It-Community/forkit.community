fbebdce574dfb5f5527c822b85a533ff1660c48a
build-ratchanon <ratchanontip@gmail.com>
2024-08-28 15:06:14 +0700

    feat: create speaker component and icon

---
+++ src/components/speaker.tsx
@@ -0,0 +1,60 @@
+import type { Speaker } from "@/content/speakers/speaker"
+import { ICONS } from "./icons"
+
+export const Speakers = (props: Readonly<{ speaker: Speaker }>) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <div
+        className="mx-auto aspect-square rounded-lg overflow-hidden"
+      >
+        <img
+          src={props.speaker.imageUrl}
+          alt={`${props.speaker.name}-profile-picture`}
+        />
+      </div>
+      <div className="flex flex-col gap-3">
+        <div>  
+          <h2 className="text-lg font-semibold leading-6 tracking-tight text-white md:text-xl">
+            {props.speaker.name}
+          </h2>
+          {!!props.speaker.job &&
+            <p className="text-neutral-400">{props.speaker.job}</p>
+          }
+          {!!props.speaker.company && (
+            <div>
+              {!!props.speaker.company.href ? (
+                <a
+                  href={props.speaker.company.href}
+                  target="_blank"
+                  className="text-sm leading-5 text-neutral-400 underline transition hover:text-primary"
+                >
+                  {props.speaker.company.title}
+                </a>
+              ) : (
+                <p className="text-sm leading-5 text-gray-400">
+                  {props.speaker.company.title}
+                </p>
+              )}
+            </div>
+          )}
+        </div>
+        {!!props.speaker.socials && (
+          <ul className="flex gap-x-2">
+            {props.speaker.socials.map((social) => (
+              <li key={social.type}>
+                <a
+                  href={social.href}
+                  className="text-gray-400 transition hover:text-primary"
+                  target="_blank"
+                >
+                  <span className="sr-only">{social.type}</span>
+                  {ICONS[social.type]}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  )
+}
