fcd1cac55a72352e494d9f263b0da503657f32a8
Yoann Fleury <yoann.fleury@yahoo.com>
2024-09-07 00:54:39 +0200

    feat: poc archi validated

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
-import { z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 import { talksCollection } from "./talks/talks";
-import { eventsCollection } from "./events/events";
+import { eventsCollection } from "./events_rename/events";
 import { organizersCollection } from "./organizers/organizers";
 import { partnersCollection } from "./partners/partners";
 import { speakersCollection } from "./speakers/speaker";
@@ -12,7 +12,20 @@ export const collections = {
   organizers: organizersCollection,
   partners: partnersCollection,
   sponsors: sponsorsCollection,
-  events: eventsCollection,
+  // events: eventsCollection,
   speaker: speakersCollection,
   talk: talksCollection,
+  events: defineCollection({
+    type: "content",
+    schema: z.object({
+      name: z.string(),
+      subpages: z.array(reference("eventsSubpages")),
+    }),
+  }),
+  eventsSubpages: defineCollection({
+    type: "content",
+    schema: z.object({
+      title: z.string(),
+    }),
+  }),
 };
