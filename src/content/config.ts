b75cee890f74b9b36d0129bd7e9bad815b6dea85
JeanneGrenet <77801791+JeanneGrenet@users.noreply.github.com>
2024-09-10 10:26:08 +0100

    fix : circular dependency with social types (#111)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,9 @@
-import { z } from "astro:content";
 import { talksCollection } from "./talks/talks";
 import { eventsCollection } from "./events/events";
 import { organizersCollection } from "./organizers/organizers";
 import { partnersCollection } from "./partners/partners";
 import { speakersCollection } from "./speakers/speaker";
 import { sponsorsCollection } from "./sponsors/sponsors";
-
-export const zSocialTypes = z.enum(["x", "linkedin", "instagram", "github"]);
 
 export const collections = {
   organizers: organizersCollection,
