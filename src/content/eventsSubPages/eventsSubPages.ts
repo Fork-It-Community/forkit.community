7c4b66bd019d8ecf976afafb8810a725b822da2a
Jeanne Grenet <jeannegrenet@MacBook-Pro-de-Jeanne.local>
2024-09-12 15:56:48 +0200

    feat : add meta title and description in layout

--- src/content/eventsSubPages/eventsSubPages.ts
+++ src/content/eventsSubPages/eventsSubPages.ts
@@ -4,6 +4,7 @@ export type EventsSubPages = z.infer<ReturnType<typeof zEventsSubPages>>;
 const zEventsSubPages = () =>
   z.object({
     title: z.string(),
+    description : z.string().optional()
   });
 
 export const eventsSubPagesCollection = defineCollection({
