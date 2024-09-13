d757f74a5083d83b77712a24e6e4568287a33ba9
JeanneGrenet <jeanne.grenet@gmail.com>
2024-09-13 11:39:31 +0200

    feat : create layouts and SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,14 @@ import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.forkit.community',
   integrations: [react(), tailwind({
     applyBaseStyles: false
-  }), mdx()]
+  }), mdx(), sitemap(), robotsTxt()]
 });
