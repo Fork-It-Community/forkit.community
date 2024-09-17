c8335a230d7cc2f6ce9b47fbcf3fd5c8300bedd3
build-ratchanon <ratchanontip@gmail.com>
2024-09-17 21:27:52 +0700

    feat: add meetup props in event card

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,33 +1,31 @@
 import type { Event } from "@/content/events/events";
+import type { Meetup } from "@/content/meetups/meetups";
 import { cn, formatTime } from "@/lib/utils";
 import { MapPin } from "lucide-react";
 
 type EventCardProps = {
-  title: Event["title"];
-  eventType: "conference" | "meetup";
-  date?: Event["date"];
-  endDate?: Event["date"];
-  location?: Event["location"];
-  image?: Event["image"];
+  event: Event | Meetup;
 };
 
 export const EventCard = (props: EventCardProps) => {
+  const eventType = 'cfp' in props.event ? "event" : "meetup";
+
   return (
     <article
       className={cn(
         "relative flex flex-col rounded-lg p-4 md:p-6",
-        props.eventType === "conference"
+        eventType === "event"
           ? "bg-cover bg-center"
           : "border border-neutral-700 bg-neutral-900",
       )}
       style={{
         backgroundImage:
-          props.eventType === "conference"
-            ? `linear-gradient(270deg, rgba(0, 0, 0, 0) 39.76%, rgba(0, 0, 0, 0.8) 100%), url(${props.image?.src})`
+          eventType === "event"
+            ? `linear-gradient(270deg, rgba(0, 0, 0, 0) 39.76%, rgba(0, 0, 0, 0.8) 100%), url(${props.event.image?.src})`
             : "none",
       }}
     >
-      {props.eventType === "conference" && (
+      {eventType === "event" && (
         <div
           className="absolute inset-0 left-0 h-full w-full rounded-lg"
           style={{
@@ -40,30 +38,30 @@ export const EventCard = (props: EventCardProps) => {
       )}
 
       <div className="relative z-10 flex flex-col justify-between gap-6 sm:flex-row">
-        {props.eventType === "conference" ? (
+        {eventType === "event" ? (
           <div className="flex flex-col gap-2">
             <div className="flex flex-row gap-4">
               <div className="flex items-center gap-2 text-sm text-neutral-400">
-                {props.date && (
-                  <time dateTime={props.date.toISOString()}>
-                    {formatTime(props.date)}
+                {props.event.date && (
+                  <time dateTime={props.event.date.toISOString()}>
+                    {formatTime(props.event.date)}
                   </time>
                 )}
-                {props.endDate && (
-                  <time dateTime={props.endDate.toISOString()}>
-                    - {formatTime(props.endDate)}
+                {('cfp' in props.event) && props.event.cfp?.endDate && (
+                  <time dateTime={props.event.cfp.endDate.toISOString()}>
+                    - {formatTime(props.event.cfp.endDate)}
                   </time>
                 )}
               </div>
             </div>
             <div className="flex flex-col font-heading text-base text-secondary">
-              {props.title}
+              {props.event.title}
             </div>
             <div>
-              {props.location && (
+              {props.event.location && (
                 <div className="flex items-center gap-2 text-sm text-neutral-400">
                   <MapPin className="h-5 w-5 flex-none" aria-hidden="true" />
-                  {props.location?.name}
+                  {props.event.location?.name}
                 </div>
               )}
             </div>
@@ -71,22 +69,22 @@ export const EventCard = (props: EventCardProps) => {
         ) : (
           <div className="flex flex-col gap-2">
             <div className="flex flex-row gap-4">
-              {props.date && (
+              {props.event.date && (
                 <div className="flex items-center gap-2 text-sm text-neutral-400">
-                  <time dateTime={props.date.toISOString()}>
-                    {formatTime(props.date)}
+                  <time dateTime={props.event.date.toISOString()}>
+                    {formatTime(props.event.date)}
                   </time>
                 </div>
               )}
-              {props.location && (
+              {props.event.location && (
                 <div className="flex items-center gap-2 text-sm text-neutral-400">
                   <MapPin className="h-5 w-5 flex-none" aria-hidden="true" />
-                  {props.location?.name}
+                  {props.event.location?.name}
                 </div>
               )}
             </div>
             <div className="flex flex-col font-heading text-base text-secondary">
-              {props.title}
+              {props.event.title}
             </div>
           </div>
         )}
