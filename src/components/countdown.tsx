8783ef796236c8e5fe6df261173d5cd3ee1a0656
build-ratchanon <ratchanontip@gmail.com>
2024-09-18 13:16:13 +0700

    feat: change locate function timer to utils file

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,69 +1,63 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import type { Event } from "@/content/events/events";
-import MotionNumber from 'motion-number';
+import MotionNumber from "motion-number";
+import { calculateTimeLeft } from "@/lib/utils";
 
 export const Countdown = (props: Readonly<{ event: Event }>) => {
   const { event } = props;
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
-  function calculateTimeLeft() {
-    const countdownDate = new Date(event?.date || 0).getTime();
-    const now = new Date().getTime();
-    const distance = countdownDate - now;
-
-    if(distance < 0) {
-      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
-    }
-
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    const formatTime = (time: number) => (time < 10 ? `0${time}` : time.toString());
-
-    return {
-      days: formatTime(days),
-      hours: formatTime(hours),
-      minutes: formatTime(minutes),
-      seconds: formatTime(seconds)
-    };
-  }
+  if (!event.date) return null;
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(event?.date));
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
+    if (!event.date) return;
+    const updateTimer = () => {
+      const formattedTime = calculateTimeLeft(event?.date as Date);
+      setTimeLeft(formattedTime);
+    };
+    const timer = setInterval(updateTimer, 1000);
+    updateTimer();
 
     return () => clearInterval(timer);
   }, [event]);
 
-  const TimerDisplay = ({value, unit} : {value: string | number, unit: "days" | "hours" | "minutes" | "seconds"}) => (
-    <div className="flex flex-col w-[66px] h-[66px] items-center bg-neutral-900 bg-opacity-40 rounded-md p-2">
-      <MotionNumber
-        value={value}
-        className='font-heading text-2xl'
-      />
-      <p className="font-heading text-xs">{unit}</p>
-    </div>
-  )
-
   return (
     <div>
-      <div className="bg-orange-300 p-5">
-        <div className="flex justify-center items-center gap-1">
-          <TimerDisplay value={timeLeft.days} unit="days" />
-          <div className="font-heading text-2xl">:</div>
-
-          <TimerDisplay value={timeLeft.hours} unit="hours" />
-          <div className="font-heading text-2xl">:</div>
-
-          <TimerDisplay value={timeLeft.minutes} unit="minutes" />
-          <div className="font-heading text-2xl">:</div>
-
-          <TimerDisplay value={timeLeft.seconds} unit="seconds" />
+      <div className="flex items-center justify-center gap-1">
+        <div className="flex h-[66px] w-[66px] flex-col items-center rounded-md bg-neutral-900 bg-opacity-40 p-2">
+          <MotionNumber
+            value={timeLeft.days}
+            className="font-heading text-2xl"
+          />
+          <p className="font-sans text-xs">Days</p>
         </div>
+        <div className="font-heading text-2xl">:</div>
 
+        <div className="flex h-[66px] w-[66px] flex-col items-center rounded-md bg-neutral-900 bg-opacity-40 p-2">
+          <MotionNumber
+            value={timeLeft.hours}
+            className="font-heading text-2xl"
+          />
+          <p className="font-sans text-xs">Hours</p>
+        </div>
+        <div className="font-heading text-2xl">:</div>
+
+        <div className="flex h-[66px] w-[66px] flex-col items-center rounded-md bg-neutral-900 bg-opacity-40 p-2">
+          <MotionNumber
+            value={timeLeft.minutes}
+            className="font-heading text-2xl"
+          />
+          <p className="font-sans text-xs">Minutes</p>
+        </div>
+        <div className="font-heading text-2xl">:</div>
+
+        <div className="flex h-[66px] w-[66px] flex-col items-center rounded-md bg-neutral-900 bg-opacity-40 p-2">
+          <MotionNumber
+            value={timeLeft.seconds}
+            className="font-heading text-2xl"
+          />
+          <p className="font-sans text-xs">Seconds</p>
+        </div>
       </div>
     </div>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -18,6 +18,30 @@ export function formatTime(date: string | number | Date) {
   }).format(new Date(date));
 }
 
+export function calculateTimeLeft(date: string | number | Date) {
+  const countdownDate = new Date(date).getTime();
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  if(distance < 0) {
+    return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+  }
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  const formatTimer = (time: number) => (time < 10 ? `0${time}` : time.toString());
+
+  return {
+    days: formatTimer(days),
+    hours: formatTimer(hours),
+    minutes: formatTimer(minutes),
+    seconds: formatTimer(seconds)
+  };
+}
+
 // TODO Uncomment when the Event type is available in the content config.
 // export function isEventInThePast(event: Event) {
 //   return (event.date?.getTime() ?? 0) < new Date().getTime();
