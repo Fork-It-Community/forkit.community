0fbbef36f5ae5cf772cff35acbe3e606d28b21cc
houssembaltii <houssem@bearstudio.fr>
2024-09-18 17:11:22 +0100

    feat :  add podcasts collection schema

---
+++ src/content/podcasts/podcasts.ts
@@ -0,0 +1,37 @@
+import { z, defineCollection } from "astro:content";
+
+export type Podcast = z.infer<ReturnType<typeof zPodcast>>;
+const zPodcast = () =>
+  z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    episodeNumber: z.number(),
+    releaseDate: z.date(),
+    duration: z.number(),
+    podcastUrls: z
+      .array(
+        z.object({
+          platform: z.string(),
+          url: z.string(),
+        }),
+      )
+      .optional(),
+    coverImageUrl: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    host: z.string(),
+    guests: z.array(z.string()),
+    language: z.enum(["french", "english"]),
+    notes: z
+      .array(
+        z.object({
+          minute: z.string(),
+          subject: z.string(),
+        }),
+      )
+      .optional(),
+  });
+
+export const podcastsCollection = defineCollection({
+  type: "content",
+  schema: zPodcast(),
+});
