649f5b13649b899331b9a38a6f2e4ba6bd06f828
Build-Ratchanon <78555958+build-ratchanon@users.noreply.github.com>
2024-09-18 14:06:22 +0700

    Feat: create component explore the community (#96)
    * feat: create component explore the community
    
    * feat: delete yarn lock
    
    * fix: change absolute import, add a tag link, add default title
    
    * fix: change primary color and change  padding x
    
    * feat: change border radius

---
+++ src/components/explore-the-community.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { ArrowUpRight } from "lucide-react";
+
+export const ExploreTheCommunity = (
+  props: Readonly<{ title?: string; href?: string }>,
+) => {
+  return (
+    <div className="flex min-h-10 flex-row items-center justify-between gap-1 rounded-lg bg-neutral-900 bg-opacity-40 p-2 pl-4">
+      <p className="text-xs font-medium">
+        {props?.title || "Fork it! Community"}
+      </p>
+      <Button asChild size="sm" className="max-h-6 text-xs px-2">
+        <a href={props?.href || "/"}>
+          Explore the community
+          <ArrowUpRight
+            className="ml-1 inline-block h-3.5 w-3.5"
+            aria-hidden="true"
+          />
+        </a>
+      </Button>
+    </div>
+  );
+};
