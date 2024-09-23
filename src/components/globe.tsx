966d2352dd2eb0014f15660fa4eebb85d4d29190
houssembaltii <houssem@bearstudio.fr>
2024-09-23 17:41:16 +0100

    feat : add globe component

---
+++ src/components/globe.tsx
@@ -0,0 +1,117 @@
+// @ts-nocheck
+import createGlobe from "cobe";
+import { useEffect, useRef } from "react";
+import { useSpring } from "react-spring";
+
+export const Globe = () => {
+  const canvasRef = useRef();
+  const pointerInteracting = useRef(null);
+  const pointerInteractionMovement = useRef(0);
+  const [{ r }, api] = useSpring(() => ({
+    r: 0,
+    config: {
+      mass: 1,
+      tension: 280,
+      friction: 40,
+      precision: 0.001,
+    },
+  }));
+  useEffect(() => {
+    let phi = 0;
+    let width = 0;
+    const onResize = () =>
+      canvasRef.current && (width = canvasRef.current.offsetWidth);
+    window.addEventListener("resize", onResize);
+    onResize();
+    const globe = createGlobe(canvasRef.current, {
+      devicePixelRatio: 1.6,
+      width: "100%",
+      height: "100%",
+      phi: 3.66,
+      theta: 0.3,
+      dark: 1,
+      diffuse: 0,
+      mapSamples: 50000,
+      mapBrightness: 6,
+      scale: 0.92,
+      baseColor: [0.251, 0.251, 0.251],
+      markerColor: [0.922, 1, 0.067],
+      glowColor: [1, 1, 1],
+      offset: [-250, 0],
+      markers: [
+        // longitude latitude
+        { location: [36.800072, 10.18706], size: 0.04 },
+        { location: [49.442402, 1.09846], size: 0.04 },
+      ],
+      onRender: (state) => {
+        // This prevents rotation while dragging
+        if (!pointerInteracting.current) {
+          // Called on every animation frame.
+          // `state` will be an empty object, return updated params.
+          phi += 0.005;
+        }
+        state.phi = phi + r.get();
+        state.width = width * 2;
+        state.height = width * 2;
+      },
+    });
+    setTimeout(() => (canvasRef.current.style.opacity = "1"));
+    return () => {
+      globe.destroy();
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+  return (
+    <div
+      style={{
+        width: "100%",
+        maxWidth: 484,
+        aspectRatio: 1,
+        margin: "auto",
+        position: "relative",
+      }}
+    >
+      <canvas
+        ref={canvasRef}
+        onPointerDown={(e) => {
+          pointerInteracting.current =
+            e.clientX - pointerInteractionMovement.current;
+          canvasRef.current.style.cursor = "grabbing";
+        }}
+        onPointerUp={() => {
+          pointerInteracting.current = null;
+          canvasRef.current.style.cursor = "grab";
+        }}
+        onPointerOut={() => {
+          pointerInteracting.current = null;
+          canvasRef.current.style.cursor = "grab";
+        }}
+        onMouseMove={(e) => {
+          if (pointerInteracting.current !== null) {
+            const delta = e.clientX - pointerInteracting.current;
+            pointerInteractionMovement.current = delta;
+            api.start({
+              r: delta / 200,
+            });
+          }
+        }}
+        onTouchMove={(e) => {
+          if (pointerInteracting.current !== null && e.touches[0]) {
+            const delta = e.touches[0].clientX - pointerInteracting.current;
+            pointerInteractionMovement.current = delta;
+            api.start({
+              r: delta / 100,
+            });
+          }
+        }}
+        style={{
+          width: "100%",
+          height: "100%",
+          cursor: "grab",
+
+          opacity: 0,
+        }}
+      />
+    </div>
+  );
+};
