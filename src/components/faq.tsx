8acb21e4adbc15a988baaa29a0c82e75c3a8228a
build-ratchanon <ratchanontip@gmail.com>
2024-09-24 17:56:09 +0700

    fix: fix format with prettier

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -3,14 +3,14 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion"
-import type { Event } from "@/content/events/events"
+} from "@/components/ui/accordion";
+import type { Event } from "@/content/events/events";
 
 export const FaqComponent = (props: Readonly<{ event: Event }>) => {
   return (
     <div className="py-16" id="faq">
       <article className="mx-auto max-w-3xl px-6 lg:px-8">
-        <h2 className="font-heading font-bold text-3xl text-center sm:text-4xl">
+        <h2 className="text-center font-heading text-3xl font-bold sm:text-4xl">
           Frequently Asked Questions
         </h2>
         <Accordion type="single" collapsible className="w-full">
@@ -18,7 +18,7 @@ export const FaqComponent = (props: Readonly<{ event: Event }>) => {
             {props.event.faq?.map((item) => (
               <AccordionItem key={item.question} value={item.question}>
                 <dt>
-                  <AccordionTrigger className="transition text-xl font-semibold hover:no-underline hover:text-neutral-400 text-left">
+                  <AccordionTrigger className="text-left text-xl font-semibold transition hover:text-neutral-400 hover:no-underline">
                     {item.question}
                   </AccordionTrigger>
                 </dt>
@@ -33,5 +33,5 @@ export const FaqComponent = (props: Readonly<{ event: Event }>) => {
         </Accordion>
       </article>
     </div>
-  )
-}
+  );
+};
