5232657a3754fb7eb56c36f0ec4b5d37de2b61e0
houssembaltii <houssem@bearstudio.fr>
2024-09-25 14:31:02 +0100

    fix :  add details about the location of the meetup

--- src/content/meetup/2024-10-17-hanoi/about.tsx
+++ src/content/meetup/2024-10-17-hanoi/about.tsx
@@ -66,13 +66,13 @@ export function About(
                   </li>
                 )}
               </ul>
-              {/*               <p>Description for the location&apos;s meetup</p>
-               */}{" "}
-              {/*  <Button variant="default" asChild className="w-fit">
-                <Link href="" target="_blank">
-                  Visit Location Website
-                </Link>
-              </Button> */}
+              {
+                <Button variant="default" asChild className="w-fit">
+                  <Link href="https://standingbarhanoi.com/" target="_blank">
+                    Visit Location Website
+                  </Link>
+                </Button>
+              }
             </div>
           </div>
         </div>
