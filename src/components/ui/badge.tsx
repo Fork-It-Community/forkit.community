8c2ae4d8cceac877c2b367147ba1ae9d9eb4a36b
Yoann Fleury <yoann.fleury@yahoo.com>
2024-09-26 12:53:46 +0200

    feat: use the same name as shadcn

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -3,17 +3,16 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 
-const tagVariants = cva(
+const badgeVariants = cva(
   "inline-flex items-center rounded-lg px-2 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
     variants: {
       variant: {
         default: "bg-neutral-900/40 text-neutral-100",
-        primary: "border-transparent bg-primary/40 text-primary-foreground",
-        secondary:
-          "border-transparent bg-secondary/40 text-secondary-foreground",
+        primary: "border-transparent bg-primary text-primary-foreground",
+        secondary: "border-transparent bg-secondary text-secondary-foreground",
         destructive:
-          "border-transparent bg-destructive/40 text-destructive-foreground",
+          "border-transparent bg-destructive text-destructive-foreground",
         outline: "text-foreground border border-foreground/50",
       },
     },
@@ -23,12 +22,14 @@ const tagVariants = cva(
   },
 );
 
-export interface TagProps
+export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof tagVariants> {}
+    VariantProps<typeof badgeVariants> {}
 
-function Tag({ className, variant, ...props }: TagProps) {
-  return <div className={cn(tagVariants({ variant }), className)} {...props} />;
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
 }
 
-export { Tag, tagVariants };
+export { Badge, badgeVariants };
