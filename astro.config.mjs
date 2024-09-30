64f5481af84e1e0b0eb10d2ed83dde0f584eb687
JeanneGrenet <jeanne.grenet@gmail.com>
2024-09-30 11:26:58 +0200

    fix : prettier and seo slot

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
-
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.forkit.community',
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  }), mdx(), sitemap(), robotsTxt()]
+  site: "https://www.forkit.community",
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+    sitemap(),
+    robotsTxt(),
+  ],
 });
