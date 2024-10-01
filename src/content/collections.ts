539b1de564dfd9ee9c566b8f722ae10552d3e9af
houssembaltii <houssem@bearstudio.fr>
2024-10-01 10:34:35 +0100

    feat : add partners to meetup collection

--- src/content/collections.ts
+++ src/content/collections.ts
@@ -199,6 +199,7 @@ const collections = {
         .optional(),
       speakers: z.array(z.string()).optional(),
       talks: z.array(z.string()).optional(),
+      partners:z.array(z.string()).optional(),
       faq: z
         .array(z.object({ question: z.string(), answer: z.string() }))
         .optional(),
