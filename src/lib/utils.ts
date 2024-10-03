673494bc5f26e3c06efa0c80e20856648eaaffdd
JeanneGrenet <jeanne.grenet@gmail.com>
2024-10-03 17:55:01 +0200

    WIP : event page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import type { Event } from "@/content/events/events";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -18,11 +19,18 @@ export function formatTime(date: string | number | Date) {
   }).format(new Date(date));
 }
 
-// TODO Uncomment when the Event type is available in the content config.
-// export function isEventInThePast(event: Event) {
-//   return (event.date?.getTime() ?? 0) < new Date().getTime();
-// }
+export function formatDateTimeShort(date: string | number | Date) {
+  return new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "2-digit",
+    year: "numeric",
+  }).format(new Date(date));
+}
 
-// export function shouldDisplayTicketButton(event: Event) {
-//   return event.tickets && !isEventInThePast(event);
-// }
+export function isEventInThePast(event: Event) {
+  return (event.date?.getTime() ?? 0) < new Date().getTime();
+}
+
+export function shouldDisplayTicketButton(event: Event) {
+  return event.tickets && !isEventInThePast(event);
+}
