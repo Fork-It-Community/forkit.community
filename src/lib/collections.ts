87b25c0f55f7b81a00f3119394836d21c3027dd3
houssembaltii <houssem@bearstudio.fr>
2024-10-07 17:08:04 +0100

    feat : update how to handle subPages slugs and subPage style

--- src/lib/collections.ts
+++ src/lib/collections.ts
@@ -1,10 +1,17 @@
 import type { Event } from "@/content/events/events";
-import { getCollection, getEntries } from "astro:content";
+import { getCollection, getEntries, type CollectionEntry } from "astro:content";
 
 export function getEventsCollection() {
   return getCollection("events");
 }
 
 export function getEventsSubpagesCollection(event: Event) {
-  return getEntries(event.subPages);
+  const subPages = event.subPages.map((subPage) => {
+    return {
+      collection:
+        "eventsSubPages" as CollectionEntry<"eventsSubPages">["collection"],
+      slug: subPage.slug,
+    };
+  });
+  return getEntries(subPages);
 }
