a58d8bbead53b1bcf66fff0810b93e87eef18d8d
IounOm <61118226+IounOm@users.noreply.github.com>
2024-10-09 11:36:52 +0700

    fix: edit from comment

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -4,25 +4,20 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import type { CollectionEntry } from "astro:content";
 
-type FaqProps = {
-  title: string;
-  list: {
-    question: string;
-    answer: string;
-  }[];
-};
-
-export const FAQ = (props: Readonly<FaqProps>) => {
+export const FAQ = (
+  props: Readonly<{ faq: CollectionEntry<"events">["data"]["faq"] }>,
+) => {
   return (
     <div className="py-16" id="faq">
       <article className="mx-auto max-w-3xl px-6 lg:px-8">
         <h2 className="text-center font-heading text-3xl font-bold sm:text-4xl">
-          {props.title}
+          Frequently Asked Questions
         </h2>
         <Accordion type="single" collapsible className="w-full">
           <dl className="mt-10">
-            {props.list.map((item) => (
+            {props.faq?.map((item) => (
               <AccordionItem key={item.question} value={item.question}>
                 <dt>
                   <AccordionTrigger className="text-left text-xl font-semibold transition hover:text-neutral-400 hover:no-underline">
