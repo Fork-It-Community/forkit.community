fd22590e526bacf3219950d47fa899b03badbfb1
IounOm <61118226+IounOm@users.noreply.github.com>
2024-10-09 16:44:31 +0700

    feat: add countdown component

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,53 +1,54 @@
 import { useState, useEffect } from "react";
-import type { Event } from "@/content/events/events";
+import type { CollectionEntry } from "astro:content";
 import MotionNumber from "motion-number";
 import { calculateTimeLeft } from "@/lib/utils";
 
-export const Countdown = (props: Readonly<{ event: Event }>) => {
-  const { event } = props;
+export const Countdown = (
+  props: Readonly<{ date: CollectionEntry<"events">["data"]["date"] }>,
+) => {
+  if (!props.date) return;
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(props.date));
 
-  if (!event.date) return;
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(event?.date));
+  const updateTimer = () => {
+    const formattedTime = calculateTimeLeft(props.date);
+    setTimeLeft(formattedTime);
+  };
 
   useEffect(() => {
-    const updateTimer = () => {
-      if (event.date) {
-        const formattedTime = calculateTimeLeft(event.date);
-        setTimeLeft(formattedTime);
-      }
-    };
     const timer = setInterval(updateTimer, 1000);
-    updateTimer();
-
     return () => clearInterval(timer);
-  }, [event]);
+  }, [props.date]);
 
-  const TimerDisplay = ({value, unit} : {value: string | number, unit: "Days" | "Hours" | "Minutes" | "Seconds"}) => {
+  const TimerDisplay = ({
+    value,
+    unit,
+  }: {
+    value: string;
+    unit: "Days" | "Hours" | "Minutes" | "Seconds";
+  }) => {
     return (
       <div className="flex h-[66px] w-[66px] flex-col items-center rounded-md bg-neutral-900 bg-opacity-40 p-2">
         <MotionNumber
+          id={unit}
           value={value}
           format={{ minimumIntegerDigits: 2 }}
           className="font-heading text-2xl"
         />
         <p className="font-sans text-xs">{unit}</p>
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <div>
       <div className="flex items-center justify-center gap-1">
-        {TimerDisplay({value: timeLeft.days, unit: "Days"})}
-        <div className="font-heading text-2xl">:</div>
-
-        {TimerDisplay({value: timeLeft.hours, unit: "Hours"})}
-        <div className="font-heading text-2xl">:</div>
-
-        {TimerDisplay({value: timeLeft.minutes, unit: "Minutes"})}
-        <div className="font-heading text-2xl">:</div>
-
-        {TimerDisplay({value: timeLeft.seconds, unit: "Seconds"})}
+        {TimerDisplay({ value: timeLeft.days, unit: "Days" })}
+        <p className="font-heading text-2xl">:</p>
+        {TimerDisplay({ value: timeLeft.hours, unit: "Hours" })}
+        <p className="font-heading text-2xl">:</p>
+        {TimerDisplay({ value: timeLeft.minutes, unit: "Minutes" })}
+        <p className="font-heading text-2xl">:</p>
+        {TimerDisplay({ value: timeLeft.seconds, unit: "Seconds" })}
       </div>
     </div>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,22 +23,24 @@ export function calculateTimeLeft(date: Date) {
   const now = new Date().getTime();
   const distance = countdownDate - now;
 
-  if(distance < 0) {
-    return { days: 0, hours: 0, minutes: 0, seconds: 0 };
-  }
+  if (distance < 0)
+    return { days: "0", hours: "0", minutes: "0", seconds: "0" };
 
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  const formatTimer = (time: number) => (time < 10 ? `0${time}` : time.toString());
+  const formatTimer = (time: number) =>
+    time < 10 ? `0${time}` : time.toString();
 
   return {
     days: formatTimer(days),
     hours: formatTimer(hours),
     minutes: formatTimer(minutes),
-    seconds: formatTimer(seconds)
+    seconds: formatTimer(seconds),
   };
 }
 
