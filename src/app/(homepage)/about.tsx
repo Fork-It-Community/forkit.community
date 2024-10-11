3209020d0d10feefde051af0d8bec45ac7ab53a8
houssembaltii <houssem@bearstudio.fr>
2024-10-11 13:44:44 +0100

    fix : image width in responsive

--- src/app/(homepage)/about.tsx
+++ src/app/(homepage)/about.tsx
@@ -13,7 +13,7 @@ export function About() {
       />
       <div className="absolute inset-0 bg-gradient-to-br from-gray-950 via-gray-950 to-gray-950/0"></div>
       <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-12 md:flex-row xl:justify-normal">
-        <div className="relative flex max-w-2xl flex-col gap-3 px-6">
+        <div className="relative mx-auto flex max-w-2xl flex-col gap-3 px-6">
           <h2 className="font-heading text-3xl font-bold sm:text-4xl">
             What is <span className="text-primary">Fork it! Community</span>?
           </h2>
@@ -26,9 +26,13 @@ export function About() {
             talk about real projects without lies for once.
           </p>
         </div>
-        <div className="h-70 relative aspect-[1134/640] w-2/3 md:w-2/4 xl:w-[45%]">
+        <div className="h-70 relative aspect-[1134/640] w-2/3 md:w-3/5 lg:w-2/4 xl:w-[45%]">
           <a href="https://www.youtube.com/watch?v=HLGnkpZ-Q1Q" target="_blank">
-            <Image className="object-cover" src={VideoThumbNail} alt="" />
+            <Image
+              className="object-cover"
+              src={VideoThumbNail}
+              alt="Fork it! Rouen After movie "
+            />
           </a>
         </div>
       </div>
