67a23d9a669aea2cfdb12c2d29b64ea20fe3585c
JeanneGrenet <jeanne.grenet@gmail.com>
2024-10-15 17:04:16 +0200

    WIP : meetup page

--- src/lib/collections.ts
+++ src/lib/collections.ts
@@ -4,6 +4,9 @@ import { getCollection, getEntries } from "astro:content";
 export function getEventsCollection() {
   return getCollection("events");
 }
+export function getMeetupsCollection() {
+  return getCollection("meetups");
+}
 
 export function getEventsSubpagesCollection(event: Event) {
   return getEntries(event.subPages);
