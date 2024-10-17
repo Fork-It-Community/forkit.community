0fac0c972d84d93bf6af4158b0eff44026dd260b
IounOm <61118226+IounOm@users.noreply.github.com>
2024-10-17 18:54:07 +0700

    fix: separate TimerDisplay to another component

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -3,7 +3,7 @@ import type { CollectionEntry } from "astro:content";
 import MotionNumber from "motion-number";
 import { calculateTimeLeft } from "@/lib/utils";
 
-export const Countdown = (
+const Countdown = (
   props: Readonly<{ date: CollectionEntry<"events">["data"]["date"] }>,
 ) => {
   if (!props.date) return;
@@ -21,37 +21,38 @@ export const Countdown = (
     return () => clearInterval(timer);
   }, [props.date]);
 
-  const TimerDisplay = ({
-    value,
-    unit,
-  }: {
-    value: string;
-    unit: "Days" | "Hours" | "Minutes" | "Seconds";
-  }) => {
-    return (
-      <div className="flex h-[66px] w-[66px] flex-col items-center rounded-md bg-neutral-900 bg-opacity-40 p-2">
-        <MotionNumber
-          id={unit}
-          value={value}
-          format={{ minimumIntegerDigits: 2 }}
-          className="font-heading text-2xl"
-        />
-        <p className="font-sans text-xs">{unit}</p>
-      </div>
-    );
-  };
-
   return (
     <div>
       <div className="flex items-center justify-center gap-1">
-        {TimerDisplay({ value: timeLeft.days, unit: "Days" })}
+        <TimerDisplay value={timeLeft.days} unit="Days" />
         <p className="font-heading text-2xl">:</p>
-        {TimerDisplay({ value: timeLeft.hours, unit: "Hours" })}
+        <TimerDisplay value={timeLeft.hours} unit="Hours" />
         <p className="font-heading text-2xl">:</p>
-        {TimerDisplay({ value: timeLeft.minutes, unit: "Minutes" })}
+        <TimerDisplay value={timeLeft.minutes} unit="Minutes" />
         <p className="font-heading text-2xl">:</p>
-        {TimerDisplay({ value: timeLeft.seconds, unit: "Seconds" })}
+        <TimerDisplay value={timeLeft.seconds} unit="Seconds" />
       </div>
     </div>
   );
 };
+
+const TimerDisplay = (
+  props: Readonly<{
+    value: string;
+    unit: "Days" | "Hours" | "Minutes" | "Seconds";
+  }>,
+) => {
+  return (
+    <div className="flex h-[66px] w-[66px] flex-col items-center rounded-md bg-neutral-900 bg-opacity-40 p-2">
+      <MotionNumber
+        id={props.unit}
+        value={props.value}
+        format={{ minimumIntegerDigits: 2 }}
+        className="font-heading text-2xl"
+      />
+      <p className="font-sans text-xs">{props.unit}</p>
+    </div>
+  );
+};
+
+export { Countdown, TimerDisplay };
