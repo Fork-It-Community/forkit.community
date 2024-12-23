10822209bdb94d6e2f6902de2b67e09cacf2e73c
Ivan Dalmet <ivan@dalmet.fr>
2024-12-23 17:35:49 +0100

    mobile nav

--- src/components/MainNav/MainNavMobileItemMore.tsx
+++ src/components/MainNav/MainNavMobileItemMore.tsx
@@ -1,13 +1,85 @@
-import { MdMoreHoriz } from "react-icons/md";
+import {
+  MdBadge,
+  MdHandshake,
+  MdLocalActivity,
+  MdMoreHoriz,
+} from "react-icons/md";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import type { FC, ReactNode } from "react";
+import { cn } from "@/lib/utils";
 
-const MainNavMobileItemMore = () => {
+const MainNavMobileItemMore = (props: { currentPathname: string }) => {
   return (
-    <button className="flex flex-1 flex-col items-center justify-center py-3 opacity-60">
+    <Drawer autoFocus>
+      <DrawerTrigger className="flex flex-1 flex-col items-center justify-center py-3 opacity-60">
+        <span className="text-2xl">
+          <MdMoreHoriz />
+        </span>
+        <span className="text-[0.6rem] tracking-wide">More</span>
+      </DrawerTrigger>
+      <DrawerContent className="pb-8">
+        <DrawerHeader>
+          <DrawerTitle className="sr-only">More</DrawerTitle>
+          <DrawerDescription className="sr-only"></DrawerDescription>
+        </DrawerHeader>
+        <MoreNavItem
+          href="/speakers"
+          currentPathname={props.currentPathname}
+          icon={MdBadge}
+        >
+          Speakers
+        </MoreNavItem>
+        <MoreNavItem
+          href="/partners"
+          currentPathname={props.currentPathname}
+          icon={MdHandshake}
+        >
+          Partners
+        </MoreNavItem>
+        <MoreNavItem
+          href="/organize-event"
+          currentPathname={props.currentPathname}
+          icon={MdLocalActivity}
+        >
+          Organize an event
+        </MoreNavItem>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+const MoreNavItem = (props: {
+  children: ReactNode;
+  icon: FC;
+  href: string;
+  exact?: boolean;
+  currentPathname: string;
+}) => {
+  const Icon = props.icon;
+  const isActive = props.exact
+    ? props.href === props.currentPathname
+    : props.currentPathname.startsWith(props.href);
+  return (
+    <a
+      href={props.href}
+      className={cn(
+        "flex items-center gap-3 px-8 py-3",
+        !isActive && "opacity-60",
+        isActive && "text-primary",
+      )}
+    >
       <span className="text-2xl">
-        <MdMoreHoriz />
+        <Icon />
       </span>
-      <span className="text-[0.6rem] tracking-wide">More</span>
-    </button>
+      <span>{props.children}</span>
+    </a>
   );
 };
 
