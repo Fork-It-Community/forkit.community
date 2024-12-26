ac4b3200e04b7380ecc2626de2bb35c66e716db5
Ivan Dalmet <ivan@dalmet.fr>
2024-12-26 16:41:35 +0100

    fix: zindex issue

--- src/components/ExploreTheCommunity/index.tsx
+++ src/components/ExploreTheCommunity/index.tsx
@@ -11,7 +11,7 @@ export const ExploreTheCommunity = (props: {
   eventDate?: Date;
 }) => {
   return (
-    <div className="bg-background-blur mx-auto flex h-12 max-w-screen-lg items-center justify-between rounded-md">
+    <div className="bg-background-blur relative z-10 mx-auto flex h-12 max-w-screen-lg items-center justify-between rounded-md">
       <a
         href={props.logoUrl}
         className="group flex min-w-0 flex-1 items-center gap-3 pe-2 ps-5 text-sm tracking-wide"
