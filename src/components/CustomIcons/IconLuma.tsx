17f4d072f7d6014e345082e5114fe8726b39b3f0
Ivan Dalmet <ivan@dalmet.fr>
2024-12-27 11:01:17 +0100

    feat: Add socials on homepage

---
+++ src/components/CustomIcons/IconLuma.tsx
@@ -0,0 +1,10 @@
+import type { SVGProps } from "react";
+
+export const IconLuma = (props: SVGProps<SVGSVGElement>) => (
+  <svg fill="none" viewBox="0 0 133 134" width="1em" height="1em" {...props}>
+    <path
+      fill="currentColor"
+      d="M133 67C96.282 67 66.5 36.994 66.5 0c0 36.994-29.782 67-66.5 67 36.718 0 66.5 30.006 66.5 67 0-36.994 29.782-67 66.5-67"
+    ></path>
+  </svg>
+);
---
+++ src/content/socials.ts
@@ -0,0 +1,67 @@
+import { IconLuma } from "@/components/CustomIcons/IconLuma";
+import type { FC, ReactNode } from "react";
+import {
+  FaBluesky,
+  FaDiscord,
+  FaGithub,
+  FaLinkedin,
+  FaXTwitter,
+  FaYoutube,
+} from "react-icons/fa6";
+
+type SocialsLevel = "primary" | "secondary";
+
+const SOCIALS: Array<{
+  icon: FC;
+  label: ReactNode;
+  href: string;
+  level: SocialsLevel;
+}> = [
+  {
+    label: "Lu.ma",
+    href: "https://lu.ma/forkit.community",
+    icon: IconLuma,
+    level: "primary",
+  },
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/company/fork-it-community",
+    icon: FaLinkedin,
+    level: "primary",
+  },
+  {
+    label: "BlueSky",
+    href: "https://bsky.app/profile/forkit.community",
+    icon: FaBluesky,
+    level: "primary",
+  },
+  {
+    label: "X Twitter",
+    href: "https://twitter.com/ForkitCommunity",
+    icon: FaXTwitter,
+    level: "primary",
+  },
+  {
+    label: "Youtube",
+    href: "https://www.youtube.com/channel/UCgV5zuiFWCMl7IvuZ5KbJOw",
+    icon: FaYoutube,
+    level: "primary",
+  },
+  {
+    label: "Discord",
+    href: "https://discord.gg/MEJ6TfJC2H",
+    icon: FaDiscord,
+    level: "secondary",
+  },
+  {
+    label: "GitHub",
+    href: "https://github.com/Fork-It-Community",
+    icon: FaGithub,
+    level: "secondary",
+  },
+];
+
+export function getSocialsItems(level?: SocialsLevel) {
+  if (!level) return SOCIALS;
+  return SOCIALS.filter((item) => item.level === level);
+}
