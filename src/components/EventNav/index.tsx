cbe1264fb012e4ef482ff1905d57854f752f2666
Ivan Dalmet <ivan@dalmet.fr>
2024-12-27 17:23:39 +0100

    feat: add ticket button in event nav

--- src/components/EventNav/index.tsx
+++ src/components/EventNav/index.tsx
@@ -25,6 +25,11 @@ export const EventNav = (props: {
   const [{ y }] = useWindowScroll();
   const { height } = useWindowSize();
   const [open, setOpen] = useState(false);
+  const canShowTicketsButton =
+    props.eventMetadata.status !== "cancelled" &&
+    props.eventMetadata.tickets?.href &&
+    dayjs().isBefore(props.eventMetadata.date);
+
   return (
     <div
       className={cn(
@@ -46,51 +51,61 @@ export const EventNav = (props: {
           </span>
         </a>
 
-        <Sheet open={open} onOpenChange={setOpen}>
-          <SheetTrigger asChild>
-            <button className="px-6 opacity-60 hover:opacity-100">
-              <MdMenu className="text-2xl" />
-            </button>
-          </SheetTrigger>
-          <SheetContent className="flex flex-col p-0">
-            <SheetHeader>
-              <SheetTitle className="sr-only">
-                {props.eventName} navigation
-              </SheetTitle>
-              <SheetDescription className="sr-only">
-                Quick access to the {props.eventName} content
-              </SheetDescription>
-            </SheetHeader>
-            <div className="flex flex-1 flex-col">
+        <div className="flex items-center gap-1">
+          {canShowTicketsButton && (
+            <Button size="xs" asChild>
               <a
-                href={`/events/${props.eventId}#`}
-                onClick={() => setOpen(false)}
-                className="group flex flex-col justify-center gap-1 px-6 py-4"
+                href={props.eventMetadata.tickets?.href}
+                className="group gap-2 max-xs:hidden"
               >
-                <Logo className="w-28" />
-                <span className="line-clamp-1 font-heading text-2xs font-medium uppercase leading-tight tracking-wide opacity-60 group-hover:opacity-100">
-                  {props.eventName}
-                </span>
+                Get Your Ticket
               </a>
-              <div className="relative flex-1">
-                <div className="absolute inset-0 overflow-auto">
-                  {props.items.map((item, index) => (
-                    <a
-                      href={item.href}
-                      onClick={() => setOpen(false)}
-                      className="flex items-center gap-3 px-6 py-3 tracking-wide opacity-80 transition hover:bg-black/20 hover:opacity-100"
-                      key={index}
-                    >
-                      {item.label}
-                    </a>
-                  ))}
+            </Button>
+          )}
+
+          <Sheet open={open} onOpenChange={setOpen}>
+            <SheetTrigger asChild>
+              <button className="self-stretch px-6 opacity-60 hover:opacity-100">
+                <MdMenu className="text-2xl" />
+              </button>
+            </SheetTrigger>
+            <SheetContent className="flex flex-col p-0">
+              <SheetHeader>
+                <SheetTitle className="sr-only">
+                  {props.eventName} navigation
+                </SheetTitle>
+                <SheetDescription className="sr-only">
+                  Quick access to the {props.eventName} content
+                </SheetDescription>
+              </SheetHeader>
+              <div className="flex flex-1 flex-col">
+                <a
+                  href={`/events/${props.eventId}#`}
+                  onClick={() => setOpen(false)}
+                  className="group flex flex-col justify-center gap-1 px-6 py-4"
+                >
+                  <Logo className="w-28" />
+                  <span className="line-clamp-1 font-heading text-2xs font-medium uppercase leading-tight tracking-wide opacity-60 group-hover:opacity-100">
+                    {props.eventName}
+                  </span>
+                </a>
+                <div className="relative flex-1">
+                  <div className="absolute inset-0 overflow-auto">
+                    {props.items.map((item, index) => (
+                      <a
+                        href={item.href}
+                        onClick={() => setOpen(false)}
+                        className="flex items-center gap-3 px-6 py-3 tracking-wide opacity-80 transition hover:bg-black/20 hover:opacity-100"
+                        key={index}
+                      >
+                        {item.label}
+                      </a>
+                    ))}
+                  </div>
                 </div>
-              </div>
-              <div className="absolute bottom-0 left-0 h-20 w-full bg-white opacity-15 blur-3xl" />
-              <div className="relative z-10 flex flex-wrap items-center justify-center gap-3 p-6">
-                {props.eventMetadata.status !== "cancelled" &&
-                  props.eventMetadata.tickets &&
-                  dayjs().isBefore(props.eventMetadata.date) && (
+                <div className="absolute bottom-0 left-0 h-20 w-full bg-white opacity-15 blur-3xl" />
+                <div className="relative z-10 flex flex-wrap items-center justify-center gap-3 p-6">
+                  {canShowTicketsButton && (
                     <Button asChild size="sm">
                       <a
                         href={props.eventMetadata.tickets?.href}
@@ -102,33 +117,34 @@ export const EventNav = (props: {
                       </a>
                     </Button>
                   )}
-                {props.eventMetadata.status !== "cancelled" &&
-                  props.eventMetadata.cfp &&
-                  dayjs().isBefore(props.eventMetadata.cfp.endDate) && (
-                    <Button asChild size="sm" variant="ghost">
-                      <a
-                        href={props.eventMetadata.cfp.href}
-                        className="flex-1"
-                        onClick={() => setOpen(false)}
-                      >
-                        CFP
-                      </a>
-                    </Button>
-                  )}
-                <Button asChild size="sm" variant="ghost">
-                  <a
-                    href="/"
-                    className="group flex w-full gap-2"
-                    onClick={() => setOpen(false)}
-                  >
-                    Community Hub
-                    <MdArrowOutward className="text-lg transition group-hover:-translate-y-0.5 group-hover:translate-x-0.5" />
-                  </a>
-                </Button>
+                  {props.eventMetadata.status !== "cancelled" &&
+                    props.eventMetadata.cfp &&
+                    dayjs().isBefore(props.eventMetadata.cfp.endDate) && (
+                      <Button asChild size="sm" variant="ghost">
+                        <a
+                          href={props.eventMetadata.cfp.href}
+                          className="flex-1"
+                          onClick={() => setOpen(false)}
+                        >
+                          CFP
+                        </a>
+                      </Button>
+                    )}
+                  <Button asChild size="sm" variant="ghost">
+                    <a
+                      href="/"
+                      className="group flex w-full gap-2"
+                      onClick={() => setOpen(false)}
+                    >
+                      Community Hub
+                      <MdArrowOutward className="text-lg transition group-hover:-translate-y-0.5 group-hover:translate-x-0.5" />
+                    </a>
+                  </Button>
+                </div>
               </div>
-            </div>
-          </SheetContent>
-        </Sheet>
+            </SheetContent>
+          </Sheet>
+        </div>
       </div>
     </div>
   );
