97754cff749275237b6f95b2f9dee49dd52bf6de
Ivan Dalmet <ivan@dalmet.fr>
2024-12-27 16:26:07 +0100

    fix: remove unused components

--- src/components/demo.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from "react";
-
-export type DemoProps = { children: ReactNode };
-
-export const Demo = ({ children }: DemoProps) => {
-  return <div className="not-prose">{children}</div>;
-};
