57929806728fa3103ad5f64238c3213fbbd560e7
Ivan Dalmet <ivan@dalmet.fr>
2024-12-30 13:02:53 +0100

    feat: smart back button

---
+++ src/components/BackButton/index.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { MdArrowBack } from "react-icons/md";
+
+export const BackButton = (props: { href: string }) => {
+  return (
+    <Button asChild variant="ghost" size="xs">
+      <a
+        href={props.href}
+        onClick={(e) => {
+          if (
+            window.history.length > 1 &&
+            document.referrer.indexOf(window.location.host) !== -1
+          ) {
+            e.preventDefault();
+            window.history.back();
+          }
+        }}
+      >
+        <MdArrowBack className="mr-2" /> Back
+      </a>
+    </Button>
+  );
+};
