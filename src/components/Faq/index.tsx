02bff99998dd558da1ebcfe896f8f5a1925fa503
Ivan Dalmet <ivan@dalmet.fr>
2025-01-10 15:53:36 +0100

    fix: style and more

--- src/components/Faq/index.tsx
+++ src/components/Faq/index.tsx
@@ -10,9 +10,12 @@ export const FAQ = (
   props: Readonly<{ faq: CollectionEntry<"events">["data"]["faq"] }>,
 ) => {
   return (
-    <div className="py-16" id="faq">
+    <div className="relative py-16 md:py-32">
       <article className="mx-auto max-w-3xl px-6 lg:px-8">
-        <h2 className="scroll-mt-32 font-heading text-2xl font-medium uppercase tracking-widest">
+        <h2
+          id="faq"
+          className="scroll-mt-32 font-heading text-2xl font-medium uppercase tracking-widest"
+        >
           Frequently Asked Questions
         </h2>
         <Accordion type="single" collapsible className="w-full">
@@ -20,7 +23,7 @@ export const FAQ = (
             {props.faq?.map((item) => (
               <AccordionItem key={item.question} value={item.question}>
                 <dt>
-                  <AccordionTrigger className="hover:text-neutral-400 text-left text-xl font-semibold transition hover:no-underline">
+                  <AccordionTrigger className="hover:text-neutral-400 text-left font-heading text-lg font-semibold tracking-wide transition hover:no-underline">
                     {item.question}
                   </AccordionTrigger>
                 </dt>
