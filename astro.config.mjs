a37c3ecdc12c874efec272dc23f63e1fa3068419
Yoann Fleury <yoann.fleury@yahoo.com>
2025-01-10 15:21:36 +0100

    feat: first redirections

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,4 +17,15 @@ export default defineConfig({
     sitemap(),
     robotsTxt(),
   ],
+  redirects: {
+    "/events/2024-06-07-rouen": "/events/2024-france-rouen",
+    "/meetups/2024-11-28-casablanca": "/events/2024-morocco-casablanca",
+    "/meetups/2024-11-15-da-nang": "/events/2024-vietnam-da-nang",
+    "/meetups/2024-11-05-bangkok": "/events/2024-thailand-bangkok",
+    "/meetups/2024-10-22-kuala-lumpur": "/events/2024-malaisia-kuala-lumpur",
+    "/meetups/2024-10-17-hanoi": "/events/2024-vietnam-hanoi",
+    "/meetups/2024-09-24-tunis": "/events/2024-tunisia-tunis",
+    "/meetups/2024-11-19-belgium":
+      "/events/2024-belgium-ottignies-louvain-la-neuve",
+  },
 });
