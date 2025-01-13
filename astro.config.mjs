b17d6549b9659dc4b194caec636a03657635d987
Yoann Fleury <yoann.fleury@yahoo.com>
2025-01-13 21:54:15 +0100

    fix: no trailing slash on build

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.forkit.community",
+  trailingSlash: "never",
   integrations: [
     react(),
     tailwind({
