41d5a5b24efb1b740b5a302d9305583dded7e968
JeanneGrenet <jeanne.grenet@gmail.com>
2025-01-21 14:10:20 +0100

    feat : improve schema and make news dynamic

--- src/schemas/news.ts
+++ src/schemas/news.ts
@@ -1,19 +1,18 @@
-import { z, reference } from "astro:content";
+import { z, reference, type SchemaContext } from "astro:content";
+
+const zNewsType = z.enum(["long", "short"]);
 
 export type News = z.infer<ReturnType<typeof zNews>>;
-export const zNews = () =>
+export const zNews = ({ image }: SchemaContext) =>
   z.object({
     title: z.string(),
     // Preview text
     description: z.string().optional(),
-    featuredImage: z
-      .object({
-        src: z.string(),
-        alt: z.string(),
-      })
-      .optional(),
+    featuredImage: image().optional(),
     authors: z.array(reference("people")),
     date: z.date(),
     tags: z.array(z.string()).optional(),
     state: z.enum(["draft", "published"]).default("draft"),
+    type: zNewsType,
+    c2a: z.string().optional(),
   });
