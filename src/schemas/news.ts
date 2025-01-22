5d7df3eca07c2cd7d7d9ca5c3cfb88db9c7b77ed
Ivan Dalmet <ivan@dalmet.fr>
2025-01-22 11:10:32 +0100

    fix: remove short news concept for now

--- src/schemas/news.ts
+++ src/schemas/news.ts
@@ -1,23 +1,13 @@
 import { z, reference, type SchemaContext } from "astro:content";
 
-const zNewsType = z.enum(["long", "short"]);
-
 export type News = z.infer<ReturnType<typeof zNews>>;
 export const zNews = ({ image }: SchemaContext) =>
   z.object({
     title: z.string(),
-    // Preview text
     excerpt: z.string().optional(),
     featuredImage: image().optional(),
     authors: z.array(reference("people")).optional(),
     date: z.date(),
     tags: z.array(z.string()).optional(),
     state: z.enum(["draft", "published"]).default("draft"),
-    type: zNewsType,
-    c2a: z
-      .object({
-        label: z.string(),
-        href: z.string(),
-      })
-      .optional(),
   });
