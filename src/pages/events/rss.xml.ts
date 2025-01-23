8705f9424e0af5c4cb030579daa07fd5f54e362c
JeanneGrenet <jeanne.grenet@gmail.com>
2025-01-23 17:54:55 +0100

    fix : remove rss on peolple, fix titles and description on event

--- src/pages/events/rss.xml.ts
+++ src/pages/events/rss.xml.ts
@@ -1,20 +1,17 @@
-import type { APIRoute } from "astro";
-import rss from "@astrojs/rss";
 import { getEventsCollection } from "@/lib/events";
-import dayjs from "dayjs";
+import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
 import { match } from "ts-pattern";
 
 export const GET: APIRoute = async function get(context) {
   const events = await getEventsCollection();
   return rss({
-    title: "Fork it! Community events",
+    title: "Fork it! Community | Events",
     description:
-      "All the Fork it! events, from meetups to full day events, come take a look at what is happening near you.",
+      "All the Fork it! events, from meetups to full day events, come take a look at what is happening near you!",
     site: context.site!,
     items: events.map((event) => ({
-      title: `${event.data.city}, ${event.data.country}, ${dayjs(event.data.date).format("YYYY")}, ${match(
-        event.data.type,
-      )
+      title: `${event.data.name}, ${match(event.data.type)
         .with("event", () => "Full Day Event")
         .with("meetup", () => "Community Meetup")
         .exhaustive()}`,
