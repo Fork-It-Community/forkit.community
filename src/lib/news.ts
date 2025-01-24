5e0757bcf7237e728f67c772e6fd6bf01e9f8d24
Yoann Fleury <yoann.fleury@yahoo.com>
2025-01-24 16:28:50 +0100

    feat: add rss and status check

--- src/lib/news.ts
+++ src/lib/news.ts
@@ -4,6 +4,7 @@ import dayjs from "dayjs";
 export async function getNewsCollection() {
   return (await getCollection("news"))
     .filter((post) => dayjs().isAfter(post.data.date))
+    .filter((post) => post.data.state === "published" || !import.meta.env.PROD)
     .sort(
       (post1, post2) =>
         (post2.data.date?.valueOf() ?? 0) - (post1.data.date?.valueOf() ?? 0),
