281c674572b423b17dd478bd690e498c79037f97
JeanneGrenet <77801791+JeanneGrenet@users.noreply.github.com>
2025-01-24 12:48:07 +0100

    Feat : add rss flows (#201)
    Co-authored-by: Yoann Fleury <yoann.fleury@yahoo.com>

---
+++ src/pages/events/rss.xml.ts
@@ -0,0 +1,26 @@
+import { getEventsCollection } from "@/lib/events";
+import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
+import { match } from "ts-pattern";
+
+export const GET: APIRoute = async function get(context) {
+  const events = await getEventsCollection();
+  return rss({
+    title: "Fork it! Community | Events",
+    description:
+      "All the Fork it! events, from meetups to full day events, come take a look at what is happening near you!",
+    site: context.site! + "/events",
+    trailingSlash: false,
+    items: events.map((event) => ({
+      title: `${event.data.name}, ${match(event.data.type)
+        .with("event", () => "Full Day Event")
+        .with("meetup", () => "Community Meetup")
+        .exhaustive()}`,
+      description: event.data.excerpt,
+      link: `${context.site}events/${event.id}`,
+      pubDate: event.data.date,
+      categories: [event.data.type],
+    })),
+    customData: `<language>en-EN</language>`,
+  });
+};
---
+++ src/pages/podcasts/rss.xml.ts
@@ -0,0 +1,20 @@
+import { getPodcastsEpisodesCollection } from "@/lib/podcasts";
+import rss from "@astrojs/rss";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async function get(context) {
+  const podcasts = await getPodcastsEpisodesCollection();
+  return rss({
+    title: "Fork it! Community | Podcasts",
+    description: "Listen to the Fork it! Community",
+    site: context.site! + "/podcasts",
+    trailingSlash: false,
+    items: podcasts.map((podcast) => ({
+      title: podcast.data.title,
+      description: podcast.data.description,
+      link: `${context.site}podcasts/${podcast.id}`,
+      pubDate: podcast.data.releaseDate,
+      categories: podcast.data.tags,
+    })),
+  });
+};
