697f0dc39714aa42f8d21d075de6ac17fc28c052
Ivan Dalmet <ivan-dalmet@users.noreply.github.com>
2025-02-05 09:40:24 +0100

    fix: improve mobile layout and fix news layout (#216)

--- src/components/BackButton/index.tsx
+++ src/components/BackButton/index.tsx
@@ -2,23 +2,37 @@ import { Button } from "@/components/ui/button";
 import type { ReactNode } from "react";
 import { MdArrowBack } from "react-icons/md";
 
-export const BackButton = (props: { href: string; children?: ReactNode }) => {
+export const BackButton = (props: {
+  href: string;
+  buttonLabel?: ReactNode;
+  contextLabel?: ReactNode;
+}) => {
   return (
-    <Button asChild variant="secondary-dark" size="xs">
-      <a
-        href={props.href}
-        onClick={(e) => {
-          if (
-            window.history.length > 1 &&
-            document.referrer.indexOf(window.location.host) !== -1
-          ) {
-            e.preventDefault();
-            window.history.back();
-          }
-        }}
-      >
-        <MdArrowBack className="mr-2" /> {props.children ?? "Back"}
-      </a>
-    </Button>
+    <div className="flex items-center gap-4">
+      <Button asChild variant="secondary-dark" size="xs">
+        <a
+          href={props.href}
+          onClick={(e) => {
+            if (
+              window.history.length > 1 &&
+              document.referrer.indexOf(window.location.host) !== -1
+            ) {
+              e.preventDefault();
+              window.history.back();
+            }
+          }}
+        >
+          <MdArrowBack className="mr-2" /> {props.buttonLabel ?? "Back"}
+        </a>
+      </Button>
+      {!!props.contextLabel && (
+        <a
+          href={props.href}
+          className="text-xs font-medium uppercase tracking-widest opacity-60 transition hover:opacity-100"
+        >
+          {props.contextLabel}
+        </a>
+      )}
+    </div>
   );
 };
