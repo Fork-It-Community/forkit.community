b2a31001e8e519b53b8d77d3963b5d4ff1e00ac2
Ivan Dalmet <ivan@dalmet.fr>
2025-02-07 17:17:06 +0100

    wip: remove old assets

--- src/pages/events/[id]/dynamic-images/_tickets-available-square.tsx
+++
@@ -1,6 +0,0 @@
-import { ticketsAvailable } from "./_tickets-available";
-export default ticketsAvailable({
-  fontScaling: 0.7,
-  width: 1080,
-  height: 1080,
-});
--- src/pages/events/[id]/talks/[talkId]/dynamic-images/[__image].[__type].ts
+++
@@ -1,7 +0,0 @@
-import { apiImageGenerator } from "@/dynamic-images";
-
-const methods = await apiImageGenerator({
-  modules: import.meta.glob("./_*.tsx", { eager: true }),
-});
-export const getStaticPaths = methods.getStaticPaths;
-export const GET = methods.GET;
