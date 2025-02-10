352557a095f9a11853af48c636540800b08f545c
Ivan Dalmet <ivan@dalmet.fr>
2025-02-10 10:19:31 +0100

    fix: news og-image

---
+++ src/pages/news/article/[id]/assets/_og-image.tsx
@@ -0,0 +1,86 @@
+import { Frame } from "@/generated-assets/components/Frame";
+import {
+  getAstroImageBase64,
+  type AssetImageConfig,
+} from "@/generated-assets/image";
+import { BgImage } from "@/generated-assets/components/BgImage";
+import { Logo } from "@/components/Logo";
+import { getNewsCollection } from "@/lib/news";
+import defaultBackgroundImage from "@/assets/images/news.jpeg";
+import dayjs from "dayjs";
+
+export const config: AssetImageConfig = {
+  width: 1920,
+  height: 1080,
+};
+
+export default async function ({ params }: { params: { id: string } }) {
+  const news = await getNewsCollection();
+  const article = news.find((n) => n.id === params.id);
+
+  if (!article) {
+    throw new Error("Not found");
+  }
+
+  const background = await getAstroImageBase64(
+    article.data.featuredImage ?? defaultBackgroundImage,
+  );
+  return (
+    <Frame {...config} style={{ padding: 128 }}>
+      <BgImage
+        src={background}
+        width={config.width}
+        height={config.height}
+        gradientAngle={10}
+      />
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 96,
+          flex: 1,
+          width: "100%",
+          justifyContent: "center",
+          zIndex: 100,
+        }}
+      >
+        <Logo style={{ width: 169 * 3, height: 18 * 3, marginTop: "auto" }} />
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 48,
+            minWidth: 0,
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              fontSize: 96,
+              fontWeight: 500,
+              lineHeight: 1,
+              marginLeft: -6, // Visual alignment
+              textWrap: "balance",
+            }}
+          >
+            {article.data.title}
+          </div>
+          <div
+            style={{
+              display: "flex",
+              fontSize: 32,
+              fontWeight: 500,
+              textTransform: "uppercase",
+              opacity: 0.6,
+              letterSpacing: 4,
+            }}
+          >
+            NEWS · {dayjs(article.data.date).format("MMMM DD, YYYY")}
+          </div>
+        </div>
+      </div>
+    </Frame>
+  );
+}
