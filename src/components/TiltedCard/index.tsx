e504a3089bf82d3943180b291b01ea7ba514e4e9
Ivan Dalmet <ivan-dalmet@users.noreply.github.com>
2025-02-28 22:15:59 +0100

    feat: tilted card (#254)

---
+++ src/components/TiltedCard/index.tsx
@@ -0,0 +1,97 @@
+import type { SpringOptions } from "motion/react";
+import { useRef, useState, type ReactNode } from "react";
+import { motion, useMotionValue, useSpring } from "motion/react";
+import { cn } from "@/lib/utils-client";
+
+interface TiltedCardProps {
+  scaleOnHover?: number;
+  rotateAmplitude?: number;
+  children?: ReactNode;
+  className?: string;
+}
+
+const springValues: SpringOptions = {
+  damping: 30,
+  stiffness: 100,
+  mass: 2,
+};
+
+export default function TiltedCard({
+  scaleOnHover = 1.05,
+  rotateAmplitude = 8,
+  children,
+  className,
+}: TiltedCardProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const rotateX = useSpring(useMotionValue(0), springValues);
+  const rotateY = useSpring(useMotionValue(0), springValues);
+  const scale = useSpring(1, springValues);
+  const opacity = useSpring(0);
+  const rotateFigcaption = useSpring(0, {
+    stiffness: 350,
+    damping: 30,
+    mass: 1,
+  });
+
+  const [lastY, setLastY] = useState(0);
+
+  function handleMouse(e: React.MouseEvent<HTMLElement>) {
+    if (!ref.current) return;
+
+    const rect = ref.current.getBoundingClientRect();
+    const offsetX = e.clientX - rect.left - rect.width / 2;
+    const offsetY = e.clientY - rect.top - rect.height / 2;
+
+    const rotationX = (offsetY / (rect.height / 2)) * -rotateAmplitude;
+    const rotationY = (offsetX / (rect.width / 2)) * rotateAmplitude;
+
+    rotateX.set(rotationX);
+    rotateY.set(rotationY);
+
+    x.set(e.clientX - rect.left);
+    y.set(e.clientY - rect.top);
+
+    const velocityY = offsetY - lastY;
+    rotateFigcaption.set(-velocityY * 0.6);
+    setLastY(offsetY);
+  }
+
+  function handleMouseEnter() {
+    scale.set(scaleOnHover);
+    opacity.set(1);
+  }
+
+  function handleMouseLeave() {
+    opacity.set(0);
+    scale.set(1);
+    rotateX.set(0);
+    rotateY.set(0);
+    rotateFigcaption.set(0);
+  }
+
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        "relative flex flex-col [perspective:800px] hover:z-10",
+        className,
+      )}
+      onMouseMove={handleMouse}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <motion.div
+        className="relative [transform-style:preserve-3d]"
+        style={{
+          rotateX,
+          rotateY,
+          scale,
+        }}
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
+}
