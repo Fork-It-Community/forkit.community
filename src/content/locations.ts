c4b4ec48cf5d5083ef7bd90e6a441b4c16873d46
izana <iza.khabouri@gmail.com>
2025-05-19 10:31:55 +0200

    feat: start get locations

---
+++ src/content/locations.ts
@@ -0,0 +1,4 @@
+export function getLocations() {
+  // TODO - parcourir chaque évènement pour en retirer soit la ville soit le pays
+  // TODO - le but étant de récupérer un nombre d'évènement réalisé par ville ou par pays
+}
