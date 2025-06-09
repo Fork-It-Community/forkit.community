7854117fde77a1d49bf8c05f0b2aebbf63f001b2
Yoann Fleury <yoann.fleury@yahoo.com>
2025-06-09 12:31:23 +0200

    feat: add logo

---
+++ src/components/LogoForKids/index.tsx
@@ -0,0 +1,127 @@
+import type { SVGProps } from "react";
+
+export const LogoForKids = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      viewBox="0 0 1026 223"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g filter="url(#filter0_ii_58_80)">
+        <path
+          d="M234.783 1.69263H1025.78V160.697L965.78 220.693H234.783V1.69263Z"
+          fill="url(#paint0_linear_58_80)"
+        />
+        <path
+          d="M294.568 160.94V61.4451H369.358V81.9651H319.813V103.025H363.148V122.06H319.813V160.94H294.568Z"
+          fill="white"
+        />
+        <path
+          d="M395.773 160.94L380.788 145.955V76.4301L395.773 61.4451H454.093L469.078 76.4301V145.955L454.093 160.94H395.773ZM410.893 141.77H439.243L443.968 137.045V85.3401L438.973 80.6151H410.893L406.033 85.3401V137.045L410.893 141.77Z"
+          fill="white"
+        />
+        <path
+          d="M487.966 160.94V61.4451H555.736L570.856 76.4301V112.07L559.516 123.41L574.636 160.94H549.256L536.161 127.055H513.211V160.94H487.966ZM513.211 107.885H541.561L546.286 103.16V85.3401L541.561 80.6151H513.211V107.885Z"
+          fill="white"
+        />
+        <path
+          d="M626.784 160.94V61.4451H652.029V101L682.809 61.4451H710.754L675.654 104.78L710.754 160.94H683.889L661.479 122.465L652.029 134.075V160.94H626.784Z"
+          fill="white"
+        />
+        <path
+          d="M718.905 160.94V141.905H729.975L729.705 80.4801H718.905V61.4451H764.805V80.4801H753.87L753.6 141.905H764.805V160.94H718.905Z"
+          fill="white"
+        />
+        <path
+          d="M782.345 160.94V61.4451H847.28L867.935 82.1001V140.285L847.28 160.94H782.345ZM807.59 141.77H836.345L843.5 134.615V87.7701L836.345 80.6151H807.59V141.77Z"
+          fill="white"
+        />
+        <path
+          d="M884.594 160.94V140.69H936.434L941.159 135.965V124.895L936.434 120.17H895.664L880.679 105.05V76.4301L895.664 61.4451H960.734V81.6951H910.244L905.519 86.4201V95.1951L910.244 99.9201H951.014L965.999 114.905V145.955L951.014 160.94H884.594Z"
+          fill="white"
+        />
+      </g>
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M78.5496 160.953L78.5496 99.3962L16.9874 160.957L1.31122 145.325L62.8956 83.7422L1.31122 83.7422L1.31122 61.2261L87.0445 61.2261L101.042 75.2233L101.042 160.953H78.5496Z"
+        fill="#F5F5F5"
+      />
+      <path
+        d="M144.783 61.2261H122.913V121.589H144.783V61.2261Z"
+        fill="#F5F5F5"
+      />
+      <path
+        d="M144.783 139.086H122.913V160.957H144.783V139.086Z"
+        fill="#F5F5F5"
+      />
+      <defs>
+        <filter
+          id="filter0_ii_58_80"
+          x="234.783"
+          y="1.69263"
+          width="795"
+          height="223"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="4" dy="4" />
+          <feGaussianBlur stdDeviation="10" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.2 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect1_innerShadow_58_80"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset />
+          <feGaussianBlur stdDeviation="35" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.3 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="effect1_innerShadow_58_80"
+            result="effect2_innerShadow_58_80"
+          />
+        </filter>
+        <linearGradient
+          id="paint0_linear_58_80"
+          x1="234.783"
+          y1="1.69263"
+          x2="1269.31"
+          y2="262.703"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#9612FF" />
+          <stop offset="1" stop-color="#7900DB" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
