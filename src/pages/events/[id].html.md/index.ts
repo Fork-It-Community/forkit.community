3ffbdb0b3264db3fda185356def88b2e9f052687
Nikola LEGAY <nikola.legay@bearstudio.fr>
2025-06-17 16:23:56 +0200

    fix : update Schedule Speakers Sponsors display

--- src/pages/events/[id].html.md/index.ts
+++ src/pages/events/[id].html.md/index.ts
@@ -2,6 +2,7 @@ import { getEventsCollection, getEventWithComputed } from "@/lib/events";
 import { ROUTES } from "@/routes.gen";
 import { lunalink, type ExtractParams } from "@bearstudio/lunalink";
 import type { APIRoute } from "astro";
+import { getEntries } from "astro:content";
 import dayjs from "dayjs";
 import { isEmpty } from "remeda";
 
@@ -24,11 +25,11 @@ ${displayVenue(event)}
 
 ${displaySchedule(event)}
 
-${displaySpeakers(event)}
+${await displaySpeakers(event)}
 
 ${displayAfterEvent(event)}
 
-${displaySponsors(event)}`,
+${await displaySponsors(event)}`,
     {
       headers: { "Content-Type": "text/plain; charset=utf-8" },
     },
@@ -52,25 +53,41 @@ ${event.data.location?.name}, ${event.data.location?.address}`;
 const displaySchedule = (event: EventWithComputed) => {
   if (isEmpty(event.data._computed.talks)) return "";
 
+  const itemsWithTime =
+    event.data.schedule?.items?.filter(
+      (item) =>
+        (item.type === "conference" || item.type === "roundtable") &&
+        item.startTime,
+    ) ?? [];
+
+  const formattedItems = itemsWithTime.map((item) =>
+    dayjs(item.startTime).format("hh:mm A"),
+  );
+
   return `## Schedule
 
 ${event.data._computed.talks
-  ?.map(
-    (item) =>
-      `- [${item?.data.title}](${lunalink(ROUTES.events[":id"].talks[":talkId"].__path, { id: event.id, talkId: item.id ?? "" })})`,
-  )
+  .map((item, index) => {
+    const timeStart = formattedItems[index];
+    return `- ${timeStart} [${item.data.title}](${lunalink(
+      ROUTES.events[":id"].talks[":talkId"].__path,
+      { id: event.id, talkId: item.id },
+    )})`;
+  })
   .join("\n")}`;
 };
 
-const displaySpeakers = (event: EventWithComputed) => {
+const displaySpeakers = async (event: EventWithComputed) => {
   if (!event.data.speakers) return "";
+
+  const speakers = await getEntries(event.data.speakers ?? []);
 
   return `## Speakers
 
-${event.data.speakers
+${speakers
   .map(
     (speaker) =>
-      `- ${speaker.id} (${lunalink(ROUTES.people[":id"].__path, { id: speaker.id ?? "" })})`,
+      `- [${speaker.data.name}](${lunalink(ROUTES.people[":id"].__path, { id: speaker.id })})`,
   )
   .join("\n")}`;
 };
@@ -104,10 +121,47 @@ ${
 }`;
 };
 
-const displaySponsors = (event: EventWithComputed) => {
+const displaySponsors = async (event: EventWithComputed) => {
   if (!event.data.sponsors) return "";
+  if (!event.data.sponsoringLevels) return "";
+
+  const silverSponsorSlugs = event.data.sponsors
+    .filter((sponsor) => sponsor.level === "SILVER")
+    .map((sponsor) => sponsor.slug);
+
+  const bronzeSponsorSlugs = event.data.sponsors
+    .filter((sponsor) => sponsor.level === "BRONZE")
+    .map((sponsor) => sponsor.slug);
+
+  const logisticSponsorSlugs = event.data.sponsors
+    .filter((sponsor) => sponsor.level === "LOGISTIC")
+    .map((sponsor) => sponsor.slug);
+
+  const sponsorSilver = await getEntries(silverSponsorSlugs);
+  const sponsorBronze = await getEntries(bronzeSponsorSlugs);
+  const sponsorLogistic = await getEntries(logisticSponsorSlugs);
 
   return `## Sponsors
-
-${event.data.sponsors.map((sponsors) => `- ${sponsors.slug.id}`).join("\n")}`;
+${
+  silverSponsorSlugs.length === 0
+    ? ""
+    : `
+### ${event.data.sponsoringLevels[0]}
+${sponsorSilver.map((sponsor) => `- [${sponsor.data.name}](${sponsor.data.href})`).join("\n")}`
+}
+${
+  bronzeSponsorSlugs.length === 0
+    ? ""
+    : `
+### ${event.data.sponsoringLevels[1]}
+${sponsorBronze.map((sponsor) => `- [${sponsor.data.name}](${sponsor.data.href})`).join("\n")}`
+}
+${
+  logisticSponsorSlugs.length === 0
+    ? ""
+    : `
+### ${event.data.sponsoringLevels[2]}
+${sponsorLogistic.map((sponsor) => `- [${sponsor.data.name}](${sponsor.data.href})`).join("\n")}`
+}
+`;
 };
