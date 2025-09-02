6364aeb32ddbf4fb9b52488803c81e372b58f5ab
Prabhu-1409 <msprabhuraj15@gmail.com>
2025-09-02 22:04:12 +0530

    Add to Calender have been Integrated (google calender)

---
+++ src/components/AddToCalender/index.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { SlCalender } from "react-icons/sl";
+
+
+interface addtoCalenderInfo{
+    startTime?:Date | undefined,
+    duration?:number | undefined ,
+    title:string,
+    location?:string
+}
+
+export const AddToCalender = ({startTime,duration,
+    title,
+    location}:addtoCalenderInfo) =>{
+
+    function toGoogleCalendarDate(date:Date) {
+  return date.toISOString().replace(/[-:]/g, "").split(".")[0] + "Z";
+}
+
+    const handleAddToCalender = () =>{
+        const start = new Date(startTime);
+        const end = new Date(start.getTime() + duration * 60000);
+        const eventUrl =
+      `https://calendar.google.com/calendar/render?action=TEMPLATE` +
+      `&text=${encodeURIComponent(title)}` +
+      `&dates=${toGoogleCalendarDate(start)}/${toGoogleCalendarDate(end)}` +
+      `&location=${encodeURIComponent(location)}`;
+        window.open(eventUrl, "_blank");
+    }
+
+    return(
+        <div className="flex items-center gap-4">
+            <Button size={'xs'}  disabled={!startTime || !duration || new Date(startTime).getTime() < Date.now()} onClick={handleAddToCalender}>
+                <SlCalender />
+            </Button>
+        </div>
+    )
+}
