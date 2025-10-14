1add7513dacf9b4b91ecb5a9921465d192959f49
JeanneGrenet <77801791+JeanneGrenet@users.noreply.github.com>
2025-10-14 10:10:45 +0200

    feat : add .html.md on news with title and content (#424)

---
+++ src/pages/news/article/[id].html.md/index.ts
@@ -0,0 +1,46 @@
+import type { ROUTES } from "@/routes.gen";
+import type { ExtractParams } from "@bearstudio/lunalink";
+import type { APIRoute } from "astro";
+import { getCollection, render, type CollectionEntry } from "astro:content";
+import { experimental_AstroContainer } from "astro/container";
+import mdxServer from "@astrojs/mdx/server.js";
+
+export const GET: APIRoute<
+  ExtractParams<(typeof ROUTES.news)[":page"]["__path"]>
+> = async ({ params }) => {
+  const news = await getCollection("news");
+  const article = news.find((article) => article.id === params.id);
+
+  if (!article) {
+    return new Response("Article not found", { status: 404 });
+  }
+
+  return new Response(
+    `${getArticleTitle(article)}\n\n${await getContent(article)}`,
+    {
+      headers: { "Content-Type": "text/markdown; charset=utf-8" },
+    },
+  );
+};
+
+export async function getStaticPaths() {
+  const news = await getCollection("news");
+  return news.map((article) => ({ params: { id: article.id } }));
+}
+
+export const getArticleTitle = (article: CollectionEntry<"news">) => {
+  return `# ${article.data.title}`;
+};
+
+export const getContent = async (article: CollectionEntry<"news">) => {
+  const { Content } = await render(article);
+
+  const container = await experimental_AstroContainer.create();
+
+  container.addServerRenderer({
+    renderer: mdxServer,
+    name: "",
+  });
+
+  return await container.renderToString(Content);
+};
