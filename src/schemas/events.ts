d542ff82ccf013f54b2e88b946e1026d7a931474
houssembaltii <houssem@bearstudio.fr>
2025-11-07 11:38:08 +0100

    feat: schedule updates when speaker withdrawl

--- src/schemas/events.ts
+++ src/schemas/events.ts
@@ -147,6 +147,9 @@ const zEventBase = ({ image }: SchemaContext) =>
                 startTime: z.date().optional(),
                 duration: z.number().optional().describe("Number of minutes"),
                 location: z.string().optional(),
+                status: z
+                  .enum(["cancelled", "replacement", "published"])
+                  .default("published"),
               }),
             )
             .optional(),
