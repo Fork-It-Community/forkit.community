d3ede5c195e999abc366ecf6826767131e1d8e96
Jeanne Grenet <jeanne.grenet@gmail.com>
2025-11-17 10:57:01 +0100

    wip: people computed

--- src/lib/people.ts
+++ src/lib/people.ts
@@ -1,4 +1,9 @@
-import { getEntry, type ReferenceDataEntry } from "astro:content";
+import {
+  getEntry,
+  type CollectionEntry,
+  type ReferenceDataEntry,
+} from "astro:content";
+import { getPersonEvents } from "./events";
 
 export async function getPeopleFromReference(
   people: Array<ReferenceDataEntry<"people">>,
@@ -6,4 +11,33 @@ export async function getPeopleFromReference(
   return Promise.all(
     people.map(async (organizer) => await getEntry(organizer)),
   );
+}
+
+export async function peopleWithComputed<
+  People extends CollectionEntry<"people">,
+>(people: People) {
+  const personEvents = await getPersonEvents(people);
+
+  const personEventsAsSpeakerCount = personEvents.filter((event) =>
+    event.data._computed.speakers.map((speaker) => speaker.id === people.id),
+  ).length;
+
+  const personEventsAsOrganizerCount = personEvents.filter((event) =>
+    event.data.organizers?.map((organizer) => organizer.id === people.id),
+  ).length;
+
+  return {
+    ...people,
+    data: {
+      ...people.data,
+      _computed: {
+        speakingCount: personEventsAsSpeakerCount,
+        organizingCount: personEventsAsOrganizerCount,
+      },
+    },
+  };
+}
+
+export async function getPeopleWithComputed(people: CollectionEntry<"people">) {
+  return await peopleWithComputed(people);
 }
