c88296a23671c79a528b4ebdfdb5f319fad1455d
Yoann Fleury <yoann.fleury@yahoo.com>
2025-11-21 12:26:24 +0100

    feat: improve style

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,7 +17,6 @@ export default {
       ring: "hsl(var(--ring))",
       background: "hsl(var(--background))",
       foreground: "hsl(var(--foreground))",
-      success: "hsl(var(--success))",
       primary: {
         DEFAULT: "hsl(var(--primary))",
         foreground: "hsl(var(--primary-foreground))",
