9eb77505f77205ccd079f679e44d76dbeecc92c4
Jeanne Grenet <jeanne.grenet@gmail.com>
2025-12-03 11:48:34 +0100

    feat : first version of the no event marketing page

--- src/routes.gen.ts
+++ src/routes.gen.ts
@@ -96,6 +96,7 @@ const ROUTES_CONFIG = {
           "_og-image": {},
           "_utils": {}
         },
+        "organize-new-event": {},
         "organizers": {
           "assets": {
             ":__image.:__type": {},
