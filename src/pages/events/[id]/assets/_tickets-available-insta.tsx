c5c4d6704116a045b4f1be62bc26e473bd85fbf4
Nikola-BS <nikola.legay@bearstudio.fr>
2025-12-10 17:34:19 +0100

    FIX : Tickets available insta (#536)

--- src/pages/events/[id]/assets/_tickets-available-insta.tsx
+++ src/pages/events/[id]/assets/_tickets-available-insta.tsx
@@ -3,7 +3,7 @@ import { ticketsAvailable } from "./_tickets-available";
 
 export const config: AssetImageConfig = {
   width: 1080,
-  height: 1080,
+  height: 1350,
 };
 
 export default ticketsAvailable({ ...config, fontScaling: 0.7 });
