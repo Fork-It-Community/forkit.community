833d00febfeadcea66add6dd0de49a1838866f51
Jeanne Grenet <jeanne.grenet@gmail.com>
2025-12-15 15:54:29 +0100

    fix: extract peolple from country and worldwide organizers logic to utils

--- src/lib/people.ts
+++ src/lib/people.ts
@@ -1,4 +1,8 @@
-import { getEntry, type ReferenceDataEntry } from "astro:content";
+import {
+  getEntry,
+  type CollectionEntry,
+  type ReferenceDataEntry,
+} from "astro:content";
 
 export async function getPeopleFromReference(
   people: Array<ReferenceDataEntry<"people">>,
@@ -7,3 +11,18 @@ export async function getPeopleFromReference(
     people.map(async (organizer) => await getEntry(organizer)),
   );
 }
+
+export const getWorldwideOrganizers = (
+  people: Array<CollectionEntry<"people">>,
+) => {
+  return people.filter(
+    (org) => org.id === "rudy-baer" || org.id === "aziz-ouertani",
+  );
+};
+
+export const getPeopleFromCountry = (
+  people: Array<CollectionEntry<"people">>,
+  countryId: CollectionEntry<"countries">["id"],
+) => {
+  return people.filter((person) => person.data.country?.id === countryId);
+};
