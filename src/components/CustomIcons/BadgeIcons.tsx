22cd67866401666f71ac9c225e1797bfac5f8a47
Jeanne Grenet <jeanne.grenet@gmail.com>
2026-01-20 14:41:02 +0100

    feat: add visual badge

---
+++ src/components/CustomIcons/BadgeIcons.tsx
@@ -0,0 +1,25 @@
+import type { AchievementLevel, AchievementSlug } from "@/lib/achievements";
+
+type BadgeIconProps = {
+  level: AchievementLevel["label"] | null;
+  slug: AchievementSlug;
+  className?: string;
+};
+
+const BadgeIcon = ({ level, slug, className }: BadgeIconProps) => {
+  if (!level) {
+    return;
+  }
+
+  return (
+    <div className={className}>
+      <img
+        src={`/src/assets/badge-icons/${level}_${slug}.svg`}
+        alt={`${level} ${slug} badge`}
+        className="h-8 w-8"
+      />
+    </div>
+  );
+};
+
+export default BadgeIcon;
