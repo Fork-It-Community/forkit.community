a05c310272bea2c7dd8e2450d9b48910960cd848
Jeanne Grenet <jeanne.grenet@gmail.com>
2026-02-13 17:44:43 +0100

    feat: use loadConfig

--- src/pages/events/[id]/assets/[__image].[__type].ts
+++ src/pages/events/[id]/assets/[__image].[__type].ts
@@ -1,8 +1,10 @@
 import { apiImageEndpoint } from "@bearstudio/astro-assets-generation";
 import type { APIRoute } from "astro";
-import "@/generated-assets/lib";
+import { loadConfig } from "@/generated-assets/lib";
 
 export const prerender = false;
+
+loadConfig();
 
 export const GET: APIRoute = apiImageEndpoint(
   import.meta.glob("./_*.tsx", { eager: true }),
