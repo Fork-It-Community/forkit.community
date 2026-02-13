c81c9473aee64e7d7eae598ae572f7d3b358f105
Jeanne Grenet <jeanne.grenet@gmail.com>
2026-02-13 17:44:43 +0100

    feat: use loadConfig

--- src/generated-assets/lib.ts
+++ src/generated-assets/lib.ts
@@ -1,27 +1,28 @@
 import { configure } from "@bearstudio/astro-assets-generation";
 
-configure({
-  debugBackground: "#0a0a0a",
-  siteUrl: import.meta.env.SITE ?? "http://localhost:4321",
-  isDev: import.meta.env.DEV,
-  customFonts: [
-    {
-      name: "Tomorrow",
-      url: "/fonts/tomorrow/Tomorrow-Regular.ttf",
-      style: "normal",
-      weight: 400,
-    },
-    {
-      name: "Tomorrow",
-      url: "/fonts/tomorrow/Tomorrow-Medium.ttf",
-      style: "normal",
-      weight: 500,
-    },
-    {
-      name: "Tomorrow",
-      url: "/fonts/tomorrow/Tomorrow-Bold.ttf",
-      style: "normal",
-      weight: 700,
-    },
-  ],
-});
+export const loadConfig = () =>
+  configure({
+    debugBackground: "#0a0a0a",
+    siteUrl: import.meta.env.SITE ?? "http://localhost:4321",
+    isDev: import.meta.env.DEV,
+    customFonts: [
+      {
+        name: "Tomorrow",
+        url: "/fonts/tomorrow/Tomorrow-Regular.ttf",
+        style: "normal",
+        weight: 400,
+      },
+      {
+        name: "Tomorrow",
+        url: "/fonts/tomorrow/Tomorrow-Medium.ttf",
+        style: "normal",
+        weight: 500,
+      },
+      {
+        name: "Tomorrow",
+        url: "/fonts/tomorrow/Tomorrow-Bold.ttf",
+        style: "normal",
+        weight: 700,
+      },
+    ],
+  });
