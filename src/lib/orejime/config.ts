dedcef0e9209e6105ba0b2ff1e4db84c7b06ff1f
Yoann Fleury <yoann.fleury@yahoo.com>
2026-02-20 11:02:51 +0100

    feat: add cookies src/lib/orejime/config.ts
    coderabbit feedback and clarity documentation: https://learn.microsoft.com/en-us/clarity/setup-and-installation/clarity-cookies#what-cookies-does-clarity-set
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/lib/orejime/config.ts
+++ src/lib/orejime/config.ts
@@ -145,7 +145,7 @@ export function getOrejimeConfig(): Config {
         title: "Analytics",
         description:
           "We use Vercel Analytics and Clarity to understand how visitors navigate our website and identify areas for improvement. This helps us provide a better user experience.",
-        cookies: ["__vercel_live_token", "_vercel_jwt", "__va"],
+        cookies: ["__vercel_live_token", "_vercel_jwt", "__va", "_clck", "_clsk"],
       },
     ],
 
