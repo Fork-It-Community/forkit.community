588452b7124a833fb133698cf91c7c3358f96c90
houssembaltii <houssem@bearstudio.fr>
2026-02-26 13:46:16 +0100

    feat: apply same logic to sponsorlogosinsta

--- src/generated-assets/components/SponsorLogos.tsx
+++ src/generated-assets/components/SponsorLogos.tsx
@@ -1,5 +1,3 @@
-import { Fragment } from "react";
-
 export const SponsorLogos = ({ logos }: { logos: string[] }) => {
   const height = 140;
   const chunkSize = logos.length <= 6 ? logos.length : 4;
@@ -67,52 +65,64 @@ export const SponsorLogos = ({ logos }: { logos: string[] }) => {
 };
 export const SponsorLogosInsta = ({ logos }: { logos: string[] }) => {
   const height = 100;
+  const chunkSize = logos.length <= 6 ? logos.length : 4;
+  const chunks: string[][] = [];
+  for (let i = 0; i < logos.length; i += chunkSize) {
+    chunks.push(logos.slice(i, i + chunkSize));
+  }
   return (
     <div
       style={{
         display: "flex",
+        flexDirection: "column",
         alignItems: "center",
         position: "relative",
         zIndex: 10,
-        flexWrap: "wrap",
         paddingBottom: 4,
-        justifyContent: "center",
       }}
     >
-      {logos.map((logo, index) => (
-        <Fragment key={logo}>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              height,
-              marginBottom: -20,
-            }}
-          >
-            <img
-              src={logo}
-              alt=""
+      {chunks.map((chunk, chunkIndex) => (
+        <div
+          key={chunkIndex}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {chunk.map((logo, index) => (
+            <div
+              key={logo}
               style={{
-                height: 130,
-                width: 130,
-                objectFit: "contain",
+                display: "flex",
+                alignItems: "center",
+                height,
+                marginBottom: -10,
               }}
-            />
-
-            {index < logos.length - 1 && (
-              <div
+            >
+              <img
+                src={logo}
+                alt=""
                 style={{
-                  fontSize: 14,
-                  opacity: 0.3,
-                  paddingLeft: 4,
-                  paddingRight: 4,
+                  height: 90,
+                  objectFit: "contain",
                 }}
-              >
-                &times;
-              </div>
-            )}
-          </div>
-        </Fragment>
+              />
+              {index < chunk.length - 1 && (
+                <div
+                  style={{
+                    fontSize: 14,
+                    opacity: 0.3,
+                    paddingLeft: 4,
+                    paddingRight: 4,
+                  }}
+                >
+                  &times;
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       ))}
     </div>
   );
