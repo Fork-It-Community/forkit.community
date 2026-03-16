e6fc55fb03f1cab37167577caa0394537652c758
Jeanne Grenet <jeanne.grenet@gmail.com>
2026-03-16 12:30:54 +0100

    feat: update middleware to redirect to next event in city

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,31 @@
 import { changeLanguage, init } from "@/i18n";
 import { defineMiddleware } from "astro:middleware";
+import { getCollection } from "astro:content";
+import { getUpcomingEvents } from "@/lib/events";
 
-export const onRequest = defineMiddleware(async ({ currentLocale }, next) => {
-  await init();
-  changeLanguage(currentLocale);
+export const onRequest = defineMiddleware(
+  async ({ currentLocale, url }, next) => {
+    await init();
+    changeLanguage(currentLocale);
 
-  return next();
-});
+    const segments = url.pathname.split("/").filter(Boolean);
+    if (segments.length === 1) {
+      const cities = await getCollection("cities");
+      const city = cities.find((c) => c.id === segments[0]);
+
+      if (city) {
+        const nextEvent = (await getUpcomingEvents()).find(
+          (e) =>
+            e.data._computed.city?.id === city.id &&
+            e.data.status === "published",
+        );
+        return Response.redirect(
+          new URL(nextEvent ? `/events/${nextEvent.id}` : "/", url),
+          302,
+        );
+      }
+    }
+
+    return next();
+  },
+);
