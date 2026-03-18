1e0505d73a7dab244a0d7466986e5160873bb2e3
Jeanne Grenet <jeanne.grenet@gmail.com>
2026-03-18 11:19:21 +0100

    feat: add vite qrcode plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+import { qrcode } from "vite-plugin-qrcode";
 
 import node from "@astrojs/node";
 import vercel from "@astrojs/vercel";
@@ -53,6 +54,9 @@ export default defineConfig({
         optional: true,
       }),
     },
+  },
+  vite: {
+    plugins: [qrcode()],
   },
 
   integrations: [
