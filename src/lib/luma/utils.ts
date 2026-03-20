342e31c0003dbd0a338dd16d1603b6288697de9f
Yoann Fleury <yoann.fleury@yahoo.com>
2026-03-20 15:05:13 +0100

    feat: add better result to avoid throws

--- src/lib/luma/utils.ts
+++ src/lib/luma/utils.ts
@@ -3,26 +3,33 @@ import type { EventComputed } from "@/lib/events";
 import { getAllGuests } from "@/lib/luma/api/guest";
 import { LUMA_API_KEY } from "astro:env/server";
 import { isNullish } from "remeda";
+import { Result } from "better-result";
 
-export function getHeaders(): HeadersInit {
+export function getHeaders(): Result<HeadersInit, Error> {
   if (!LUMA_API_KEY) {
-    throw new Error("LUMA_API_KEY is not set");
+    return Result.err(new Error("LUMA_API_KEY is not set"));
   }
-  return {
+
+  return Result.ok({
     accept: "application/json",
     "x-luma-api-key": LUMA_API_KEY,
-  };
+  });
 }
 
 export async function getNumberOfApprovedGuests(event: EventComputed) {
-  if (!LUMA_API_KEY || isNullish(event.data.lumaEventId)) {
+  if (isNullish(event.data.lumaEventId)) {
     return DEFAULT_NUMBER_OF_GUESTS[event.data.type];
   }
+
   const guests = await getAllGuests({
     event_api_id: event.data.lumaEventId,
   });
 
-  const guestNumber = guests.filter(
+  if (guests.isErr()) {
+    return DEFAULT_NUMBER_OF_GUESTS[event.data.type];
+  }
+
+  const guestNumber = guests.value.filter(
     (guest) => guest.approval_status === "approved",
   ).length;
   return Math.max(DEFAULT_NUMBER_OF_GUESTS[event.data.type], guestNumber);
