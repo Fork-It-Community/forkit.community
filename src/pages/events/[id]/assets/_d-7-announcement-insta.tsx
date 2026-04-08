049b26954276f2e55fa9f94264e58adba4e8d059
Jeanne Grenet <jeanne.grenet@gmail.com>
2026-04-08 16:09:24 +0200

    fix: migrate last days assets

--- src/pages/events/[id]/assets/_d-7-announcement-insta.tsx
+++ src/pages/events/[id]/assets/_d-7-announcement-insta.tsx
@@ -164,13 +164,13 @@ export function d7announcementInsta(options: {
                 viewBox="0 0 24 24"
                 style={{
                   flex: "none",
-                  opacity: 0.6,
+                  opacity: 0.85,
                   width: "1em",
                   height: "1em",
                 }}
               >
                 <path
-                  fill="currentColor"
+                  fill="white"
                   d="M19 19H5V8h14m-3-7v2H8V1H6v2H5c-1.11 0-2 .89-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2h-1V1m-1 11h-5v5h5z"
                 />
               </svg>
@@ -193,13 +193,13 @@ export function d7announcementInsta(options: {
                   viewBox="0 0 24 24"
                   style={{
                     flex: "none",
-                    opacity: 0.6,
+                    opacity: 0.85,
                     width: "1em",
                     height: "1em",
                   }}
                 >
                   <path
-                    fill="currentColor"
+                    fill="white"
                     d="M12 11.5A2.5 2.5 0 0 1 9.5 9A2.5 2.5 0 0 1 12 6.5A2.5 2.5 0 0 1 14.5 9a2.5 2.5 0 0 1-2.5 2.5M12 2a7 7 0 0 0-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 0 0-7-7"
                   />
                 </svg>
