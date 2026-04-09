a2cead4f6e420bfdbbf1a1271e21f5a4dabea5e7
Jeanne Grenet <jeanne.grenet@gmail.com>
2026-04-09 15:59:20 +0200

    fix: remove image buffer to base 64 in qr code

--- src/generated-assets/image.ts
+++ src/generated-assets/image.ts
@@ -1,7 +1,3 @@
-export function imageBufferToBase64(buffer: Buffer, fileType: string) {
-  return `data:image/${fileType};base64, ${buffer.toString("base64")}`;
-}
-
 export function getImageNameFromTsxPath(path: string) {
   return path
     .split("/")
