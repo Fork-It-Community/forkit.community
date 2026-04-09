4d871b9fd67196d7050e61824a24c6aaf0144717
Jeanne Grenet <jeanne.grenet@gmail.com>
2026-04-09 10:00:23 +0200

    fix: migrate and improve cfp and registration assets

--- src/pages/events/[id]/assets/_cfp-still-open.tsx
+++ src/pages/events/[id]/assets/_cfp-still-open.tsx
@@ -2,7 +2,7 @@ import { Frame } from "@/generated-assets/components/Frame";
 import {
   getAstroImageBase64,
   type AssetImageConfig,
-} from "@/generated-assets/image";
+} from "@bearstudio/astro-assets-generation";
 import { BgImage } from "@/generated-assets/components/BgImage";
 import { COLORS } from "@/generated-assets/theme";
 import { getEventDisplayDate } from "@/lib/events";
@@ -136,8 +136,9 @@ export function cfpStillOpen(options: {
             <div
               style={{
                 display: "flex",
-                flexDirection: "column",
-                gap: 16,
+                flexWrap: "wrap",
+                alignItems: "center",
+                rowGap: 48,
               }}
             >
               <div
@@ -147,25 +148,25 @@ export function cfpStillOpen(options: {
                   gap: 12,
                   fontSize: 48 * options.fontScaling,
                   fontWeight: 500,
-                  opacity: 0.6,
+                  lineHeight: 1,
                 }}
               >
                 <svg
                   viewBox="0 0 24 24"
                   style={{
-                    opacity: 0.6,
+                    flex: "none",
+                    opacity: 0.8,
                     width: "1em",
                     height: "1em",
                   }}
                 >
                   <path
-                    fill="currentColor"
+                    fill="white"
                     d="M19 19H5V8h14m-3-7v2H8V1H6v2H5c-1.11 0-2 .89-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2h-1V1m-1 11h-5v5h5z"
                   />
                 </svg>
                 {getEventDisplayDate(event)}
               </div>
-
               <div
                 style={{
                   display: "flex",
@@ -173,25 +174,26 @@ export function cfpStillOpen(options: {
                   alignItems: "center",
                   fontSize: 48 * options.fontScaling,
                   fontWeight: 500,
-                  opacity: 0.6,
                 }}
               >
                 <svg
                   viewBox="0 0 24 24"
                   style={{
                     flex: "none",
-                    opacity: 0.6,
+                    opacity: 0.85,
                     width: "1em",
                     height: "1em",
                   }}
                 >
                   <path
-                    fill="currentColor"
+                    fill="white"
                     d="M12 11.5A2.5 2.5 0 0 1 9.5 9A2.5 2.5 0 0 1 12 6.5A2.5 2.5 0 0 1 14.5 9a2.5 2.5 0 0 1-2.5 2.5M12 2a7 7 0 0 0-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 0 0-7-7"
                   />
                 </svg>
-                {event.data._computed.city?.data.name},{" "}
-                {event.data._computed.country?.data.name}
+                <span>
+                  {event.data._computed.city?.data.name},{" "}
+                  {event.data._computed.country?.data.name}
+                </span>
               </div>
             </div>
 
--- src/pages/events/[id]/assets/_registration-still-open-insta.tsx
+++ src/pages/events/[id]/assets/_registration-still-open-insta.tsx
@@ -2,7 +2,7 @@ import { Frame } from "@/generated-assets/components/Frame";
 import {
   getAstroImageBase64,
   type AssetImageConfig,
-} from "@/generated-assets/image";
+} from "@bearstudio/astro-assets-generation";
 import { BgImage } from "@/generated-assets/components/BgImage";
 import { COLORS } from "@/generated-assets/theme";
 import { getEventDisplayDate, getEventDisplayType } from "@/lib/events";
@@ -143,13 +143,13 @@ export function registrationStillOpenInsta(options: {
                 <svg
                   style={{
                     flex: "none",
-                    opacity: 0.6,
+                    opacity: 0.85,
                     width: "1em",
                     height: "1em",
                   }}
                   viewBox="0 0 24 24"
                   fill="none"
-                  stroke="currentColor"
+                  stroke="white"
                   strokeWidth="2"
                 >
                   <path d="M8 2v4M16 2v4M3 10h18M5 4h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2z" />
@@ -157,35 +157,37 @@ export function registrationStillOpenInsta(options: {
                 {getEventDisplayDate(event)}
               </div>
 
-              <div
-                style={{
-                  display: "flex",
-                  gap: 12,
-                  alignItems: "center",
-                  fontSize: 48,
-                  fontWeight: 500,
-                  lineHeight: 1.2,
-                  textWrap: "balance",
-                }}
-              >
-                <svg
+              {!!event.data.location?.name && (
+                <div
                   style={{
-                    flex: "none",
-                    opacity: 0.6,
-                    width: "1em",
-                    height: "1em",
+                    display: "flex",
+                    gap: 12,
+                    alignItems: "center",
+                    fontSize: 45,
+                    fontWeight: 500,
+                    lineHeight: 1,
+                    textWrap: "balance",
                   }}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
                 >
-                  <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
-                  <circle cx="12" cy="10" r="3" />
-                </svg>
-                {event.data._computed.city?.data.name},{" "}
-                {event.data._computed.country?.data.name}
-              </div>
+                  <svg
+                    style={{
+                      flex: "none",
+                      opacity: 0.85,
+                      width: "1em",
+                      height: "1em",
+                    }}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="white"
+                    strokeWidth="2"
+                  >
+                    <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
+                    <circle cx="12" cy="10" r="3" />
+                  </svg>
+
+                  {event.data.location.name}
+                </div>
+              )}
             </div>
           </div>
           <div
