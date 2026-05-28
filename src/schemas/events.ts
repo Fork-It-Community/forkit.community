a8fcdc3be9c74a3909792aa0e7b29a400800f5b7
jeannegrenet <jeanne.grenet@gmail.com>
2026-05-28 14:32:36 +0200

    feat: event vibes photo component

--- src/schemas/events.ts
+++ src/schemas/events.ts
@@ -76,8 +76,6 @@ export const zEventBasicInfo = ({ image }: SchemaContext) =>
                 })
                 .optional(),
             )
-
-            .max(4)
             .optional(),
         }),
         rating: z
